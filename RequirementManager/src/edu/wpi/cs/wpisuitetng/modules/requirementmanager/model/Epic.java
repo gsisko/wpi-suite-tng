702a34e07f6ceb5e75cb36bc6457e0cc35a5dbac
Robert Dabrowski <rpdabrowski@wpi.edu>
2013-03-16 02:01:17 -0400

    Development of uniqueID for Epic
    --  Comments for various instances of uniqueID in the epic class
    --  Getters and setters for uniqueID
    --  Javadocs for the Epic class

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/model/Epic.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/model/Epic.java
@@ -1,12 +1,16 @@
 package edu.wpi.cs.wpisuitetng.modules.requirementmanager.model;
 
+
 public class Epic extends Requirement {
 	
 	private String name;
 	private String description;
-
+	private int uniqueID; // We need to discuss how to implement this.. possibly migrate to requirement... probably controlled on server end
+	
+	
 	public Epic(RequirementPriority priority, String name, String description) {
 		super(priority);
+		this.setUniqueID(-1); // (-1) will be a flag to the server/database that this value needs to be set
 		this.setName(name);
 		this.setDescription(description);
 		// TODO Auto-generated constructor stub
@@ -15,6 +19,7 @@ public class Epic extends Requirement {
 	public Epic(RequirementPriority priority, int releaseNumber, String name, String description) {
 		super(priority);this.setName(name);
 		this.setDescription(description);
+		this.setUniqueID(-1);    // (-1) will be a flag to the server/database that this value needs to be set
 		// TODO Auto-generated constructor stub
 	}
 
@@ -41,21 +46,53 @@ public class Epic extends Requirement {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	/**
+	 *  
+	 * @return The name of this epic
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * 
+	 * @param name The name of this epic
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	/**
+	 * 
+	 * @return The description of this epic
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * 
+	 * @param description The description of this epic
+	 */
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	/**
+	 * 
+	 * @return The unique identifier for this Epic
+	 */
+	public int getUniqueID() {
+		return uniqueID;
+	}
+
+	/**
+	 * 
+	 * @param uniqueID The unique identifier for this Epic
+	 */
+	public void setUniqueID(int uniqueID) {
+		this.uniqueID = uniqueID;
 	}
 
 }
