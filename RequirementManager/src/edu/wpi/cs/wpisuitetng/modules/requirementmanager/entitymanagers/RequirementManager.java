d10620b9ac270e3a1cb9a314c6135aa27a1cca03
Robert Dabrowski <rpdabrowski@wpi.edu>
2013-03-16 21:28:01 -0400

    Create RequirementManager and Message
    -- Create RequirementMessage - used for encoding a requirement into a
    string, then decoding it back to a REQ
    -- Comments and Methods for ReqManager

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/entitymanagers/RequirementManager.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/entitymanagers/RequirementManager.java
@@ -7,24 +7,63 @@ import edu.wpi.cs.wpisuitetng.exceptions.ConflictException;
 import edu.wpi.cs.wpisuitetng.exceptions.NotFoundException;
 import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
 import edu.wpi.cs.wpisuitetng.modules.EntityManager;
+import edu.wpi.cs.wpisuitetng.modules.postboard.model.PostBoardMessage;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
 
-public class RequirementManager implements EntityManager<Requirement> {
+/**This is the entity manager for the RequirementMessage in the 
+ * RequirementManager module
+ * 
+ * @author Calder
+ * @author Dabrowski
+ *
+ */
 
+
+
+public class RequirementManager implements EntityManager<Requirement> {
+	/** The database */
 	Data db;
 
+	/**
+	 * Constructs the entity manager. This constructor is called by
+	 * {@link edu.wpi.cs.wpisuitetng.ManagerLayer#ManagerLayer()}. 
+	 * To make sure this happens, be sure to place add this entity 
+	 * manager to the map in the ManagerLayer file.
+	 * 
+	 * @param db a reference to the persistent database
+	 */	
 	public RequirementManager(Data data) {
 		db = data;
 	}
 
-	@Override
+	
+	
+	
+	/** Makes the an entity
+	 *
+	 *	@param s The current session
+	 *	@param content The message that comes in the form of a string to be recreated
+	 *	@return 
+	 */
 	public Requirement makeEntity(Session s, String content)
 			throws BadRequestException, ConflictException, WPISuiteException {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
+		// Parse the message from JSON
+		final Requirement newReq = RequirementMessage.fromJson(content);
+
+		// Save the message in the database if possible, otherwise throw an exception
+		// We want the message to be associated with the project the user logged in to
+		if (!db.save(newReq, s.getProject())) {
+				throw new WPISuiteException();
+		}
+
+		// Return the newly created message (this gets passed back to the client)
+		return newReq;
+	}
+		
+	
+	
+	
 	public Requirement[] getEntity(Session s, String id) throws NotFoundException, WPISuiteException {
 		// TODO Auto-generated method stub
 		return null;
---
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/entitymanagers/RequirementMessage.java
@@ -0,0 +1,70 @@
+package edu.wpi.cs.wpisuitetng.modules.requirementmanager.entitymanagers;
+
+import com.google.gson.Gson;
+
+import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.old.Epic;
+
+/**
+ * @author Dabrowski
+ *
+ */
+public class RequirementMessage extends AbstractModel{
+
+	/**
+	 * 
+	 */
+	public RequirementMessage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	/**
+	 * Converts this Epic to a JSON string
+	 * @return a string in JSON representing this Epic
+	 */
+	public String toJSON1() {
+		String json;
+		Gson gson = new Gson();
+		json = gson.toJson(this, Epic.class);
+		return json;
+	}
+	
+	
+	
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toJSON() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean identify(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
+
+	public static Requirement fromJson(String content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
