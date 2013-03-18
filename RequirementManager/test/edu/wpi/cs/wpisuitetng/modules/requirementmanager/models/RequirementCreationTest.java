763d45ac6b156c46df8d633d935dce2c04811af3
Robert Smieja <robertsmieja@gmail.com>
2013-03-18 16:35:59 -0400

    Start of test creation
    -Templates made for SaveRequirementControllerTest and
    RequirementCreationTest

---
+++ RequirementManager/test/edu/wpi/cs/wpisuitetng/modules/requirementmanager/models/RequirementCreationTest.java
@@ -0,0 +1,264 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.requirementmanager.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Robert Smieja
+ *
+ */
+public class RequirementCreationTest {
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#setProject(edu.wpi.cs.wpisuitetng.modules.core.models.Project)}.
+	 */
+	@Test
+	public final void testSetProject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#Requirement(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public final void testRequirementStringString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#Requirement(int, java.lang.String, java.lang.String, edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.RequirementPriority)}.
+	 */
+	@Test
+	public final void testRequirementIntStringStringRequirementPriority() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#save()}.
+	 */
+	@Test
+	public final void testSave() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#delete()}.
+	 */
+	@Test
+	public final void testDelete() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#toJSON()}.
+	 */
+	@Test
+	public final void testToJSON() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#identify(java.lang.Object)}.
+	 */
+	@Test
+	public final void testIdentify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#toJSON(edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement[])}.
+	 */
+	@Test
+	public final void testToJSONRequirementArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#fromJSON(java.lang.String)}.
+	 */
+	@Test
+	public final void testFromJSON() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#fromJSONArray(java.lang.String)}.
+	 */
+	@Test
+	public final void testFromJSONArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#addGsonDependencies(com.google.gson.GsonBuilder)}.
+	 */
+	@Test
+	public final void testAddGsonDependencies() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#getId()}.
+	 */
+	@Test
+	public final void testGetId() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#setId(int)}.
+	 */
+	@Test
+	public final void testSetId() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#getActualEffort()}.
+	 */
+	@Test
+	public final void testGetActualEffort() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#setActualEffort(int)}.
+	 */
+	@Test
+	public final void testSetActualEffort() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#getEstimate()}.
+	 */
+	@Test
+	public final void testGetEstimate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#setEstimate(int)}.
+	 */
+	@Test
+	public final void testSetEstimate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#getDescription()}.
+	 */
+	@Test
+	public final void testGetDescription() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#setDescription(java.lang.String)}.
+	 */
+	@Test
+	public final void testSetDescription() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#getName()}.
+	 */
+	@Test
+	public final void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#setName(java.lang.String)}.
+	 */
+	@Test
+	public final void testSetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#getReleaseNumber()}.
+	 */
+	@Test
+	public final void testGetReleaseNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#setReleaseNumber(int)}.
+	 */
+	@Test
+	public final void testSetReleaseNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#getStatus()}.
+	 */
+	@Test
+	public final void testGetStatus() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#setStatus(edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.RequirementStatus)}.
+	 */
+	@Test
+	public final void testSetStatus() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#getPriority()}.
+	 */
+	@Test
+	public final void testGetPriority() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#setPriority(edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.RequirementPriority)}.
+	 */
+	@Test
+	public final void testSetPriority() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#getEvents()}.
+	 */
+	@Test
+	public final void testGetEvents() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#setEvents(java.util.List)}.
+	 */
+	@Test
+	public final void testSetEvents() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement#updateReq(edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement)}.
+	 */
+	@Test
+	public final void testUpdateReq() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
