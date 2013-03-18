046832c09fb6712fc0db32e5e31a5c7232b25956
Robert Dabrowski <rpdabrowski@wpi.edu>
2013-03-17 20:54:26 -0400

    Create assignUniqueID and make getEntity
    -- make and implement assignUniqueID - only does so if necessary,
    implemented in save() method
    -- make getEntity - needs testing!

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/entitymanagers/RequirementManager.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/entitymanagers/RequirementManager.java
@@ -39,19 +39,19 @@ public class RequirementManager implements EntityManager<Requirement> {
 	 *  Requirement and saves it in the database
 	 *  
 	 *	@param s The current user session
-	 *	@param content The message that comes in the form of a string to be recreated
+	 *	@param content The requirement that comes in the form of a string to be recreated
 	 *	@return the Requirement that originally came as a string
 	 */
 	public Requirement makeEntity(Session s, String content)
 			throws BadRequestException, ConflictException, WPISuiteException {
 
-		// Parse the message from JSON
+		// Parse the requirement from JSON
 		final Requirement newRequirement = Requirement.fromJSON(content);
 
-		// Saves the message in the database if possible
+		// Saves the requirement in the database if possible
 		this.save(s,newRequirement); 
 
-		// Return the newly created message (this gets passed back to the client)
+		// Return the newly created requirement (this gets passed back to the client)
 		return newRequirement;
 	}
 		
@@ -63,11 +63,11 @@ public class RequirementManager implements EntityManager<Requirement> {
 	public Requirement[] getAll(Session s) throws WPISuiteException {
 		// Ask the database to retrieve all objects of the type Requirement.
 		// Passing a dummy Requirement lets the db know what type of object to retrieve
-		// Passing the project makes it only get messages from that project
-		List<Model> messages = db.retrieveAll(new Requirement("",""), s.getProject());
+		// Passing the project makes it only get requirements from that project
+		List<Model> requirements = db.retrieveAll(new Requirement("",""), s.getProject());
 
-		// Return the list of messages as an array
-		return messages.toArray(new Requirement[0]);
+		// Return the list of requirements as an array
+		return requirements.toArray(new Requirement[0]);
 	}
 	
 	
@@ -78,13 +78,25 @@ public class RequirementManager implements EntityManager<Requirement> {
 	 * 
 	 */
 	public void save(Session s, Requirement model) throws WPISuiteException {
-		// Save the message in the database if possible, otherwise throw an exception
-		// We want the message to be associated with the project the user logged in to
+		assignUniqueID(model); // Assigns a unique ID to the Req if necessary
+		
+		// Save the requirement in the database if possible, otherwise throw an exception
+		// We want the requirement to be associated with the project the user logged in to
 		if (!db.save(model, s.getProject())) {
 			throw new WPISuiteException();
 		}
 	}
 
+    /** Takes a Requirement and assigns a unique id if necessary
+     * 
+     * @param req The requirement that possibly needs a unique id
+     * @throws WPISuiteException If there are no Requirements in the database
+     */
+    public void assignUniqueID(Requirement req) throws WPISuiteException{
+        if (req.getId() == -1){// -1 is a flag that says a unique id is needed            
+            req.setId(this.Count() + 1); // Makes first Requirement have id = 1
+        }        
+    }
 
 	/** Returns the number of Requirements currently in the database. Disregards
 	 *  the current user session
@@ -96,10 +108,7 @@ public class RequirementManager implements EntityManager<Requirement> {
 		return db.retrieveAll(new Requirement("","")).size();
 	}
 
-
-// Unimplemented Manager methods	
-	
-	
+// TODO    THIS NEEDS TESTING!!!!	
 	/**  For the current user session, Takes a specific id for a Requirement and returns it 
 	 *   in an array.	
 	 *  
@@ -108,13 +117,32 @@ public class RequirementManager implements EntityManager<Requirement> {
 	 *  @return An array of Requirements 
  	 */
 	public Requirement[] getEntity(Session s, String id) throws NotFoundException, WPISuiteException {
-		// TODO Auto-generated method stub
+		// Ask the database to retrieve all objects of the type Requirement.
+		// Passing a dummy Requirement lets the db know what type of object to retrieve
+		// Passing the project makes it only get requirements from that project
+		List<Model> requirements = db.retrieveAll(new Requirement("",""), s.getProject());
 		
+		// Iterate through the list to find the Requirement with the correct ID
+		// Casting is used because the retrieveAll function returns a list of "Models"
+		// even though we know it is returning a list of Requirements here
+	    for ( Model r : requirements ) {
+	    	if ((   (Requirement) r).getId() == Integer.parseInt( id) ){
+	    		// Since the requirement has to be returned as an element in an array,
+	    		// we make an array of size one and add the Requirement to the array.
+	    		Requirement[] desiredReq = new Requirement[1];
+	    		desiredReq[0] = (Requirement) r; 
+	    		return desiredReq; 
+	    	}
+	    }		
 		// Throw an exception if an ID was specified but not found
-		//throw new NotFoundException();
-		return null;
+		throw new NotFoundException();
 	}
+
 	
+
+	// Unimplemented Manager methods	
+			
+
 	
 	
 	// TODO    figure out how we might update a Requirement..
