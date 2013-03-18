a115a7fc100fa99df2c593cd86c8d81c43136660
Arsenic <arsenic@Dolicho.(none)>
2013-03-18 16:25:47 -0400

    Added a missing method into the requirement panel to fetch the name text field. Commented my code thoroughly and completely.

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/views/RequirementPanel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/views/RequirementPanel.java
@@ -39,18 +39,19 @@ import javax.swing.JLabel;
 @SuppressWarnings("serial")
 public class RequirementPanel extends JPanel {
 	
-
-	private final JTextArea txtDescription;
-	private final JTextField txtName;
-
-
-	private final JButton btnSave;
+	//The labels
+	private final JLabel nameLabel; //The label for the name text field ("txtName")
+	private final JLabel descriptionLabel;//The label for the description text area ("txtDescription")
 	
-	private final JLabel nameLabel;
-	private final JLabel descriptionLabel;
+	//The text field/areas
+	private final JTextArea txtDescription;//The description text area
+	private final JTextField txtName;//The name text field 
 
-
+	//The button
+	private final JButton btnSave; //The "save" button
+	
 	/**
+	 * The constructor for RequirementPanel;
 	 * Construct the panel, the components, and add the
 	 * components to the panel.
 	 */
@@ -70,84 +71,97 @@ public class RequirementPanel extends JPanel {
             public void actionPerformed(ActionEvent e)
             {
                 //Execute when button is pressed
-                System.out.println("You clicked the button");//to be replaced
+                System.out.println("You clicked the button");//TODO: replace this with a real action
             }
         });      
 		
 		// Set the layout manager of this panel that controls the positions of the components
-		setLayout(new GridBagLayout());
-		GridBagConstraints constraints = new GridBagConstraints();
+		setLayout(new GridBagLayout()); //set the layout
+		GridBagConstraints constraints = new GridBagConstraints();//create the constraints variable
 		
-		// Put the txtDescription in a scroll pane
-		JScrollPane scrollPane = new JScrollPane(txtDescription);
-		scrollPane.setPreferredSize(new Dimension(500,400));
 		
-		// Clear the contents of the text field when the user clicks on it
+		JScrollPane scrollPane = new JScrollPane(txtDescription);// Put the txtDescription in a scroll pane
+		scrollPane.setPreferredSize(new Dimension(500,400)); //Set the initial size of the txtDescription scroll pane
+		
+		// Clear the contents of the description text area when the user clicks on it
 		txtDescription.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				txtDescription.setText("");
 			}
 		});
 		
-		// Clear the contents of the text field when the user clicks on it
+		// Clear the contents of the name text field when the user clicks on it
 		txtName.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				txtName.setText("");
 				}
 		});
 		
-		// Adjust sizes and alignments
-		btnSave.setAlignmentX(Component.CENTER_ALIGNMENT);
+		// Adjust the size and alignments of all the components and add them to the view:
+		btnSave.setAlignmentX(Component.CENTER_ALIGNMENT); //set the alignment of the "save" button to center in it's section
 
-
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.weightx = 0.25;
-		constraints.anchor = GridBagConstraints.PAGE_START;
-		constraints.insets = new Insets(10,0,0,0);  //top padding
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		add(nameLabel, constraints);
+		//Set the constraints for "nameLabel" and add it to the view
+		constraints.fill = GridBagConstraints.HORIZONTAL; //This sets the constraints of this field so that the item will stretch horizontally to fill it's area
+		constraints.weightx = 0.25; //This is the weight of this field, which tells the layout manager how big this field should be in proportion to the other components
+		constraints.anchor = GridBagConstraints.PAGE_START;//This sets the anchor of the field, here we have told it to anchor the component to the top center of it's field
+		constraints.insets = new Insets(10,0,0,0);  //Set the top padding to 10 units of blank space
+		constraints.gridx = 0; //set the x coord of the cell of the layout we are describing
+		constraints.gridy = 0;//set the y coord of the cell of the layout we are describing
+		add(nameLabel, constraints);//Actually add the "nameLabel" to the layout given the previous constraints
 		
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.weightx = 0.75;
-		constraints.insets = new Insets(10,5,0,0);  //side padding
-		constraints.gridx = 1;
-		constraints.gridwidth = 2;   //2 columns wide
-		constraints.gridy = 0;
-		add(txtName, constraints);
+		//Set the constraints for "txtName" and add it to the view
+		constraints.fill = GridBagConstraints.HORIZONTAL;//This sets the constraints of this field so that the item will stretch horizontally to fill it's area
+		constraints.weightx = 0.75;//This is the weight of this field, which tells the layout manager how big this field should be in proportion to the other components
+		constraints.insets = new Insets(10,5,0,0);  //Set the padding; here, there will be 10 units of blank space padding on the top and 5 on the left side
+		constraints.gridx = 1;//Set the x coord of the cell of the layout we are describing
+		constraints.gridwidth = 2; //Tell the layout that this field will fill 2 columns
+		constraints.gridy = 0;//Set the y coord of the cell of the layout we are describing
+		add(txtName, constraints);//Actually add the "txtName" to the layout given the previous constraints
 		
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.ipady = 70;
-		constraints.weightx = 0.25;
-		constraints.anchor = GridBagConstraints.PAGE_START; 
-		constraints.insets = new Insets(10,0,0,0);  //top padding
-		constraints.gridx = 0;
-		constraints.gridy = 1;
-		add(descriptionLabel, constraints);
+		//Set the constraints for the "descriptionLabel" and add it to the view
+		constraints.fill = GridBagConstraints.HORIZONTAL;//This sets the constraints of this field so that the item will stretch horizontally to fill it's area
+		constraints.ipady = 70;//This tells the layout to stretch this field vertically by 70 units
+		constraints.weightx = 0.25;//This is the weight of this field, which tells the layout manager how big this field should be in proportion to the other components
+		constraints.anchor = GridBagConstraints.PAGE_START; //This sets the anchor of the field, here we have told it to anchor the component to the top center of it's field
+		constraints.insets = new Insets(10,0,0,0);  //Set the top padding to 10 units  of blank space
+		constraints.gridx = 0;//Set the x coord of the cell of the layout we are describing
+		constraints.gridy = 1;//Set the y coord of the cell of the layout we are describing
+		add(descriptionLabel, constraints);//Actually add the "txtName" to the layout given the previous constraints
 		
-		constraints.fill = GridBagConstraints.BOTH;
-		constraints.weightx = 0.75;
-		constraints.insets = new Insets(10,5,0,0);  //side padding
-		constraints.gridx = 1;
-		constraints.gridwidth = 2;   //2 columns wide
-		constraints.gridy = 1;
-		add(scrollPane, constraints);
+		//Set the constraints for the "scrollPane" containing the "txtDescription" and add it to the view
+		constraints.fill = GridBagConstraints.BOTH;//This sets the constraints of this field so that the item will stretch both horizontally and vertically to fill it's area
+		constraints.weightx = 0.75;//This is the weight of this field, which tells the layout manager how big this field should be in proportion to the other components
+		constraints.insets = new Insets(10,5,0,0); //Set the padding; here, there will be 10 units of blank space padding on the top and 5 on the left side
+		constraints.gridx = 1;//Set the x coord of the cell of the layout we are describing
+		constraints.gridwidth = 2;   //Tell the layout that this field will fill 2 columns
+		constraints.gridy = 1;//Set the y coord of the cell of the layout we are describing
+		add(scrollPane, constraints);//Actually add the "scrollPane" to the layout given the previous constraints
 		
-		constraints.fill = GridBagConstraints.NONE;
-		constraints.ipady = 0;
-		constraints.weighty = 1;
-		constraints.anchor = GridBagConstraints.PAGE_END; //bottom of space
-		constraints.insets = new Insets(10,0,0,0);  //top padding
-		constraints.gridx = 2;
-		constraints.gridy = 2;
-		add(btnSave, constraints);
+		//Set the constraints for the "btnSave" and add it to the view
+		constraints.fill = GridBagConstraints.NONE;//This sets the constraints of this field so that the item will not stretch to fill it's area
+		constraints.ipady = 0;//This tells the layout to reset the vertical ipad from the previously defined 70 units to now 0 units
+		constraints.weighty = 1;//This is the weight of this field, which tells the layout manager how big this field should be in proportion to the other components
+		constraints.anchor = GridBagConstraints.PAGE_END; //This sets the anchor of the field, here we have told it to anchor the component to the bottom right of it's field
+		constraints.insets = new Insets(10,0,0,0);//Set the top padding to 10 units
+		constraints.gridx = 2;//Set the x coord of the cell of the layout we are describing
+		constraints.gridy = 2;//Set the y coord of the cell of the layout we are describing
+		add(btnSave, constraints);//Actually add the "btnSave" to the layout given the previous constraints
 		
 	}
 	
 	/**
+	 * This returns the contents of the JTextArea "txtDescription"
 	 * @return the txtDescription JTextArea
 	 */
 	public JTextArea getTxtNewMessage() {
 		return txtDescription;
+	}
+	
+	/**
+	 * This returns the contents of the JTextField "txtName"
+	 * @return the txtName JTextField
+	 */
+	public JTextField getNameTxtNewMessage() {
+		return txtName;
 	}
 }
