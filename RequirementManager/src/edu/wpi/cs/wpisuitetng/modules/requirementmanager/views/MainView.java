a5f2297be7cce989c0f728b871fb3afc0d745d1e
Paula Rudy <paularudy@gmail.com>
2013-03-18 16:35:05 -0400

    Added a missing method into the requirement panel to fetch the name text field. Commented my code thoroughly and completely.

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/views/MainView.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/views/MainView.java
@@ -1,1 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Paula Rudy
+ ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.requirementmanager.views;
+
+import javax.swing.JPanel;
+
+/**
+ * This panel fills the main content area of the tab for this module. It
+ * contains one inner JPanel, the RequirementPanel.
+ * 
+ * @author Paula Rudy
+ *
+ */
+@SuppressWarnings("serial")
+public class MainView extends JPanel {
+
+	/** The panel containing the requirement panel */
+	private final RequirementPanel reqPanel;
+	
+	/**
+	 * Construct the requirement panel and add it to the current view.
+	 */
+	public MainView() {
+		reqPanel = new RequirementPanel();//Actually create the panel
+		add(reqPanel);// Add the panel to this view
+	}
+}
