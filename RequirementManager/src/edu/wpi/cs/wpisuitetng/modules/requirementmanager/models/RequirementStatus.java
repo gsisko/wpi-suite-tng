0ba1379dad7b5ba772d6d79e1ee15d385e8d07cc
Robert Dabrowski <rpdabrowski@wpi.edu>
2013-03-18 21:13:39 -0400

    add methods to RequirementStatus
    -- toStatus - converts string to enum
    -- toString - converts enum to string

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/models/RequirementStatus.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/models/RequirementStatus.java
@@ -8,5 +8,29 @@ public enum RequirementStatus {
 	IN_PROGRESS,  // The first value after being new. Also the value when a requirement is part of an iteration
 	OPEN,     // The status of a requirement that is in the backlog
 	COMPLETE,  // A complete Requirement
-	DELETED	// Any of these status's may lead to DELETED, but DELETED may move to any but NEW
-}     // Need to ask Marty about this, but some of the project descriptions hint at this
+	DELETED;	// Any of these status's may lead to DELETED, but DELETED may move to any but NEW
+	 // Need to ask Marty about this, but some of the project descriptions hint at this
+	
+
+	/** Converts a string to the proper RequirementStatus 
+	 * 
+	 * !NOTE: This method is designed to only take strings retrieved from a UI.dropdownlist that containts
+	 * only the enums listed as RequirementStatus's
+	 * 
+	 * @param toConvert The string you want to convert
+	 * @return  The proper RequirementStatus
+	 */
+    public RequirementStatus toStatus(String toConvert){     
+       return valueOf(toConvert);
+ 
+    }
+    
+    /** Converts a RequirementStatus to a string
+     * 
+     * @param toConvert The RequirementStatus to convert to a string
+     * @return the resulting string
+     */
+    public String toString(){
+         return this.toString();
+     }    
+}    
