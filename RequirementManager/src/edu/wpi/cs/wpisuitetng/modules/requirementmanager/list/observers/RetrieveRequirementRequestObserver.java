77fdaf0bfc44e34a03a2f4ca6fb74fccee44b588
Mike Calder <Mike@Mike-Calders-MacBook-Pro.local>
2013-03-20 11:19:53 -0400

    Small fix
    - stop using the id, use row number

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/observers/RetrieveRequirementRequestObserver.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/observers/RetrieveRequirementRequestObserver.java
@@ -14,15 +14,15 @@ public class RetrieveRequirementRequestObserver implements RequestObserver {
 
 	/** The retrieve requirement controller using this observer */
 	protected RetrieveRequirementController controller;
-	protected int id;
+	protected int row;
 
 	/**
 	 * Construct a new observer
 	 * @param controller the controller managing the request
 	 */
-	public RetrieveRequirementRequestObserver(RetrieveRequirementController controller, int id) {
+	public RetrieveRequirementRequestObserver(RetrieveRequirementController controller, int row) {
 		this.controller = controller;
-		this.id = id;
+		this.row = row;
 	}
 
 	@Override
@@ -41,8 +41,8 @@ public class RetrieveRequirementRequestObserver implements RequestObserver {
 
 		// parse the requirement received from the core
 		Requirement[] requirements = Requirement.fromJSONArray(response.getBody());
-		if (requirements.length > 0 && requirements[id-1] != null) {
-			controller.showRequirement(requirements[id-1]);
+		if (requirements.length > 0 && requirements[row] != null) {
+			controller.showRequirement(requirements[row]);
 		}
 		else {
 			controller.errorRetrievingRequirement("No requirements received.");
