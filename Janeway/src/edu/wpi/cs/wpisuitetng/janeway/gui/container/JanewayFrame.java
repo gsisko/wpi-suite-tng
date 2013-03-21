c5e778c341ac464cd2f47d9018df9c3a4c58616e
dmlarose <dmlarose93@gmail.com>
2013-03-20 22:46:22 -0400

    Make min size bigger

--- Janeway/src/edu/wpi/cs/wpisuitetng/janeway/gui/container/JanewayFrame.java
+++ Janeway/src/edu/wpi/cs/wpisuitetng/janeway/gui/container/JanewayFrame.java
@@ -73,7 +73,7 @@ public class JanewayFrame extends JFrame {
 		int yPos = (int)((dim.height - height) / 2 * .75);
 		setBounds(xPos, yPos, width, height);
 		this.setPreferredSize(new Dimension(width, height));
-		this.setMinimumSize(new Dimension(1150,600));
+		this.setMinimumSize(new Dimension(1150,700));
 		
 		// Setup the layout manager
 		this.setLayout(new BorderLayout());
