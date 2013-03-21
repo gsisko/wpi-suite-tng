6084d9ec19eb128ad558195d0a30ea7f227e5c12
bangyan zhang <udarshen@gmail.com>
2013-03-21 19:45:17 -0400

    observer commented

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/controllers/SaveRequirementObserver.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/controllers/SaveRequirementObserver.java
@@ -7,7 +7,7 @@ import edu.wpi.cs.wpisuitetng.network.models.ResponseModel;
 
 /**
  * This observer is called when a response is received from a request
- * to the server to add a message.
+ * to the server to save a message. 
  *
  */
 public class SaveRequirementObserver implements RequestObserver {
@@ -38,12 +38,16 @@ public class SaveRequirementObserver implements RequestObserver {
 		// auto-update
 		rmcontroller.getView().getRefreshController().refreshData();
 	}
-
+	/* This method responses when there is a save response error
+	 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseError(edu.wpi.cs.wpisuitetng.network.models.IRequest)
+	 */
 	@Override
 	public void responseError(IRequest iReq) {
 		System.err.println("Cannot save a requirement.");
 	}
-
+	/*This method responses when the save action failed 
+	 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#fail(edu.wpi.cs.wpisuitetng.network.models.IRequest, java.lang.Exception)
+	 */
 	@Override
 	public void fail(IRequest iReq, Exception exception) {
 		System.err.println("Fail: Cannot save a requirement.");
