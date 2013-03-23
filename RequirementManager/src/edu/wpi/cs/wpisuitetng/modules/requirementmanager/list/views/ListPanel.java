7834845d788267c187f00cc0c81ce8d44c413e8f
clhudson <clhudson@wpi.edu>
2013-03-23 15:44:35 -0400

    more implementing toolbar
    all the java problems are related to a filter, main tab controller,
    main tab view, tabs, retrieve all requirements controller, and retrieve
    requirements controller, which are in progress by other team members

---
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/views/ListPanel.java
@@ -0,0 +1,80 @@
+
+
+package edu.wpi.cs.wpisuitetng.modules.requirementmanager.list.views;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SpringLayout;
+
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.tabs.MainTabController;
+
+/**
+ * Panel to hold the three portions of the requirement list interface. The
+ * list of saved filters is displayed in {@link FilterListPanel}, the filter
+ * builder is displayed in {@link FilterBuilderPanel}, and the results of
+ * the list are displayed in {@link ResultsPanel}.
+ */
+@SuppressWarnings("serial")
+public class ListPanel extends JPanel {
+
+	/** Panel containing the filter building interface */
+	protected FilterBuilderPanel builderPanel;
+
+	/** Panel containing the list of filters saved by the user */
+	protected FilterListPanel filtersPanel;
+
+	/** Panel containing the results of the requirement list */
+	protected ResultsPanel resultsPanel;
+
+	/** The layout manager for this panel */
+	protected SpringLayout layout;
+	
+	/** The main tab controller */
+	protected MainTabController tabController;
+
+	/**
+	 * Constructs the list panel and sets up the layout for the sub-panels
+	 * @param tabController The main tab controller
+	 */
+	public ListPanel(MainTabController tabController) {
+		this.tabController = tabController;
+		
+		// Set the layout manager of this panel
+		this.layout = new SpringLayout();
+		this.setLayout(layout);
+
+		// Construct the panels that compose the list view
+		this.builderPanel = new FilterBuilderPanel();
+		JScrollPane builderScrollPane = new JScrollPane(builderPanel);
+		this.filtersPanel = new FilterListPanel();
+		JScrollPane filtersScrollPane = new JScrollPane(filtersPanel);
+		this.resultsPanel = new ResultsPanel(tabController);
+
+		// Constrain the filtersPanel
+		layout.putConstraint(SpringLayout.NORTH, filtersScrollPane, 0, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.WEST, filtersScrollPane, 0, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.SOUTH, filtersScrollPane, 0, SpringLayout.SOUTH, this);
+		layout.putConstraint(SpringLayout.EAST, filtersScrollPane, 200, SpringLayout.WEST, filtersScrollPane);
+		
+		// Constrain the builderPanel
+		layout.putConstraint(SpringLayout.NORTH, builderScrollPane, 0, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.WEST, builderScrollPane, 0, SpringLayout.EAST, filtersScrollPane);
+		layout.putConstraint(SpringLayout.EAST, builderScrollPane, 0, SpringLayout.EAST, this);
+		layout.putConstraint(SpringLayout.SOUTH, builderScrollPane, 150, SpringLayout.NORTH, builderScrollPane);
+		
+		// Constrain the resultsPanel
+		layout.putConstraint(SpringLayout.NORTH, resultsPanel, 0, SpringLayout.SOUTH, builderScrollPane);
+		layout.putConstraint(SpringLayout.WEST, resultsPanel, 0, SpringLayout.EAST, filtersScrollPane);
+		layout.putConstraint(SpringLayout.EAST, resultsPanel, 0, SpringLayout.EAST, this);
+		layout.putConstraint(SpringLayout.SOUTH, resultsPanel, 0, SpringLayout.SOUTH, this);
+
+		// Add the panels
+		this.add(filtersScrollPane);
+		this.add(builderScrollPane);
+		this.add(resultsPanel);
+	}
+	
+	public ResultsPanel getResultsPanel() {
+		return resultsPanel;
+	}
+}
---
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/views/ListRequirementsView.java
@@ -0,0 +1,92 @@
+
+
+package edu.wpi.cs.wpisuitetng.modules.requirementmanager.list.views;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+
+import edu.wpi.cs.wpisuitetng.janeway.gui.container.toolbar.IToolbarGroupProvider;
+import edu.wpi.cs.wpisuitetng.janeway.gui.container.toolbar.ToolbarGroupView;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.list.controllers.RefreshRequirementsAction;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.list.controllers.RetrieveAllRequirementsController;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.tabs.MainTabController;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.tabs.Tab;
+
+/**
+ * View that contains the entire requirement listing interface
+ */
+@SuppressWarnings("serial")
+public class ListRequirementsView extends JPanel implements IToolbarGroupProvider {
+	
+	/** Panel containing the list interface */
+	protected ListPanel mainPanel;
+	
+	/** The layout manager for this panel */
+	protected SpringLayout layout;
+	
+	/** The panel containing buttons for the toolbar */
+	protected ToolbarGroupView buttonGroup;
+	
+	/** The refresh button that reloads the results of the list/filter */
+	protected JButton btnRefresh;
+	
+	/** Controller to handle list and filter requests from the user */
+	protected RetrieveAllRequirementsController controller;
+	
+	/** The main tab controller */
+	protected MainTabController tabController;
+	
+	/**
+	 * Construct the view
+	 * @param tabController The main tab controller
+	 * @param tab The Tab containing this view
+	 */
+	public ListRequirementsView(MainTabController tabController, Tab tab) {
+		this.tabController = tabController;
+		
+		if(tab != null) {
+			tab.setTitle("List Requirements");
+			tab.setIcon(new ImageIcon());
+			tab.setToolTipText("List for requirements");
+		}
+		
+		mainPanel = new ListPanel(tabController);
+		
+		// Construct the layout manager and add constraints
+		layout = new SpringLayout();
+		this.setLayout(layout);
+		layout.putConstraint(SpringLayout.NORTH, mainPanel, 0, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.SOUTH, mainPanel, 0, SpringLayout.SOUTH, this);
+		layout.putConstraint(SpringLayout.WEST, mainPanel, 0, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.EAST, mainPanel, 0, SpringLayout.EAST, this);
+		
+		// Add the mainPanel to this view
+		this.add(mainPanel);
+		
+		// Instantiate the controller
+		controller = new RetrieveAllRequirementsController(this);
+		
+		// Instantiate the button panel
+		buttonGroup = new ToolbarGroupView("List/Filter");
+		
+		// Instantiate the refresh button
+		btnRefresh = new JButton();
+		btnRefresh.setAction(new RefreshRequirementsAction(controller));
+		buttonGroup.getContent().add(btnRefresh);
+		buttonGroup.setPreferredWidth(150);
+		
+		// Load initial data
+		controller.refreshData();
+	}
+	
+	public ListPanel getListPanel() {
+		return mainPanel;
+	}
+
+	@Override
+	public ToolbarGroupView getGroup() {
+		return buttonGroup;
+	}
+}
