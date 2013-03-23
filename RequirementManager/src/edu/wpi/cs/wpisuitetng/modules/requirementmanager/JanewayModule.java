fcdf5e802bc220fac93a6bc3252a68231b425d42
Mike Calder <Mike@mrcalder-w.wifi.wpi.edu>
2013-03-23 16:06:08 -0400

    Add Tab Controller To Janeway (Chris)
    - added mainTabController to JanewayModule file
    - this is Chris from Mike's laptop

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/JanewayModule.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/JanewayModule.java
@@ -8,6 +8,8 @@ import javax.swing.JPanel;
 
 import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.tabs.MainTabController;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.tabs.MainTabView;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.requirement.RequirementPanel;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.toolbar.ToolbarView;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.toolbar.ToolbarController;
@@ -22,20 +24,23 @@ public class JanewayModule implements IJanewayModule {
 	/** The tabs used by this module */
 	private ArrayList<JanewayTabModel> tabs;
 	
-	
-	public ToolbarController toolbarController;
+	public final MainTabController mainTabController;
+	public final ToolbarController toolbarController;
 	
 	/**
 	 * Construct a new DummyModule for demonstration purposes
 	 */
 	public JanewayModule() {
 		
+		MainTabView mainTabView = new MainTabView();
+		mainTabController = new MainTabController(mainTabView);
+		
 		// Setup toolbar view and controller
 		ToolbarView toolbarView = new ToolbarView(mainTabController);
 		toolbarController = new ToolbarController(toolbarView, mainTabController);
 		
 		// Setup the main panel
-		RequirementPanel mainPanel = new RequirementPanel();
+		RequirementPanel mainPanel = new RequirementPanel(null,null,null); //TODO: change as soon as possible
 		
 		tabs = new ArrayList<JanewayTabModel>();
 		JanewayTabModel tab = new JanewayTabModel("Requirement Manager", new ImageIcon(), toolbarView, mainPanel);
