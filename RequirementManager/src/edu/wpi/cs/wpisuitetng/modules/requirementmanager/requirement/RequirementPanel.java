cbea78aba91f5e5b6aeda291aef140684cb239ab
Robert Dabrowski <rpdabrowski@wpi.edu>
2013-03-24 15:53:39 -0400

    - fix merge for Paula
    -- random merge conflicts- result of multiple people committing and
    working on the same fiel at the same time

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/requirement/RequirementPanel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/requirement/RequirementPanel.java
@@ -42,14 +42,14 @@ import edu.wpi.cs.wpisuitetng.modules.requirementmanager.views.JNumberTextField;
  */
 @SuppressWarnings("serial")
 public class RequirementPanel extends JPanel {
-	
+
 	//An enum to store the mode of the current instance of this panel
 	public enum Mode {
 		CREATE,//When Mode is this value, we are creating a new requirement
 		EDIT//When Mode is this value, we are editing an existing requirement
 	}
-	
-	
+
+
 	//The labels
 	private final JLabel nameLabel; //The label for the name text field ("txtName")
 	private final JLabel descriptionLabel;//The label for the description text area ("txtDescription")
@@ -59,7 +59,7 @@ public class RequirementPanel extends JPanel {
 	private final JLabel releaseNumLabel; //The label for the release number text field ("txtReleaseNum")
 	private final JLabel estimateLabel; //The label for the estimate text field ("txtEstimate")
 	private final JLabel actualEffortLabel; //The label for the actual effort text field ("txtActualEffort")
-	
+
 	//The fillable components
 	private final JTextField txtName;//The name text field 
 	private final JTextArea txtDescription;//The description text area
@@ -73,10 +73,10 @@ public class RequirementPanel extends JPanel {
 	//The variables to hold information about the current instance of the panel
 	private Requirement currentRequirement;//Stores the requirement currently open for editing or creation
 	private RequirementView parent; //Stores the RequirementView that contains the panel
-	
-	
+
+
 	private Mode mode;// The variable to store the enum indicating whether or not you are creating at the time
-	
+
 	/**
 	 * The constructor for RequirementPanel;
 	 * Construct the panel, the components, and add the
@@ -86,11 +86,11 @@ public class RequirementPanel extends JPanel {
 	 * @param editMode	The mode of the current panel. This is "CREATE" when we are creating a new requirement, and "EDIT" when we are editing an existing requirement.
 	 */
 	public RequirementPanel(RequirementView view, Requirement requirement, Mode editMode) {
-		
+
 		parent = view;//Set the RequirementView that contains this instance of this panel
 		currentRequirement = requirement; //Set the requirement that is currently open for editing or creation
 		mode = editMode;//Set the indicated mode
-	
+
 		//Construct the labels
 		nameLabel = new JLabel("Name:");
 		descriptionLabel = new JLabel("Description:");
@@ -100,7 +100,7 @@ public class RequirementPanel extends JPanel {
 		releaseNumLabel = new JLabel("ReleaseNum:");
 		estimateLabel = new JLabel("Estimate:");
 		actualEffortLabel = new JLabel("ActualEffort:");
-		
+
 		//Construct the misc components
 		txtName = new JTextField(/*"Enter a name here."*/"");
 		txtDescription = new JTextArea(/*"Enter a description here."*/"", 2, 2);
@@ -112,28 +112,28 @@ public class RequirementPanel extends JPanel {
 		txtReleaseNum.setAllowNegative(false);
 		txtEstimate.setAllowNegative(false);
 		txtActualEffort.setAllowNegative(false);
-		
+
 		//Set the following fields to be initially greyed out
 		txtName.setEnabled(false);
 		txtDescription.setEnabled(false);
 		txtReleaseNum.setEnabled(false);
 		txtEstimate.setEnabled(false);
 		txtActualEffort.setEnabled(false);
-		
+
 		//Set the txtDescription component to wrap
 		txtDescription.setLineWrap(true);
 		txtDescription.setWrapStyleWord(true);
-		
+
 		//Create the strings for the boxes
 		String[] typeStrings = { " ", "Epic", "Theme", "UserStory", "NonFunctional", "Scenario" };
 		String[] statusStrings = { "New", "InProgress", "Open", "Complete", "Deleted" };
 		String[] priorityStrings = { " ", "High", "Medium", "Low"};
-		
+
         //Construct the boxes 
 		typeBox = new JComboBox<String>(typeStrings);
 		statusBox = new JComboBox<String>(statusStrings);
 		priorityBox = new JComboBox<String>(priorityStrings);
-		
+
 		//Set the initial selections for the boxes
 		typeBox.setSelectedIndex(0);
 		typeBox.setEnabled(false);
@@ -141,15 +141,15 @@ public class RequirementPanel extends JPanel {
 		statusBox.setEnabled(false);
 		priorityBox.setSelectedIndex(0);
 		priorityBox.setEnabled(false);
-		
+
 		// Set the layout manager that controls the positions of the components
 		setLayout(new GridBagLayout()); //set the layout
 		GridBagConstraints reqPanelConstraints = new GridBagConstraints();//create the constraints variable
-		
+
 		//Set up the description scroll pane
 		JScrollPane scrollPane = new JScrollPane(txtDescription);// Put the txtDescription in a scroll pane
 		scrollPane.setPreferredSize(new Dimension(400,100)); //Set the initial size of the txtDescription scroll panel
-		
+
 		//In this last section we adjust the size and alignments of all the components and add them to the panel
 		// Please read all the comments in this section if you are having trouble understanding what is going on.
 
@@ -170,7 +170,7 @@ public class RequirementPanel extends JPanel {
 		reqPanelConstraints.gridy = 0;
 		add(txtName, reqPanelConstraints);
 		//end Name
-		
+
 		//Description:		
 		//Set the constraints for the "descriptionLabel" and add it to the view
 		reqPanelConstraints.fill = GridBagConstraints.HORIZONTAL;
@@ -189,7 +189,7 @@ public class RequirementPanel extends JPanel {
 		reqPanelConstraints.gridy = 1;
 		add(scrollPane, reqPanelConstraints);
 		//end Description
-		
+
 		//Type:
 		//Set the constraints for the "typeLabel" and add it to the view
 		reqPanelConstraints.fill = GridBagConstraints.HORIZONTAL;
@@ -208,7 +208,7 @@ public class RequirementPanel extends JPanel {
 		reqPanelConstraints.gridy = 2;
 		add(typeBox, reqPanelConstraints);
 		//end Type
-		
+
 		//Status:		
 		//Set the constraints for the "statusLabel" and add it to the view
 		reqPanelConstraints.fill = GridBagConstraints.HORIZONTAL;
@@ -226,7 +226,7 @@ public class RequirementPanel extends JPanel {
 		reqPanelConstraints.gridy = 3;
 		add(statusBox, reqPanelConstraints);
 		//end Status
-		
+
 		//Priority:
 		//Set the constraints for the "priorityLabel" and add it to the view
 		reqPanelConstraints.fill = GridBagConstraints.HORIZONTAL;
@@ -244,7 +244,7 @@ public class RequirementPanel extends JPanel {
 		reqPanelConstraints.gridy = 4;
 		add(priorityBox, reqPanelConstraints);
 		//end Priority
-		
+
 		//Release number:
 		//Set the constraints for the "releaseNumLabel" and add it to the view
 		reqPanelConstraints.fill = GridBagConstraints.HORIZONTAL;
@@ -262,7 +262,7 @@ public class RequirementPanel extends JPanel {
 		reqPanelConstraints.gridy = 5;
 		add(txtReleaseNum, reqPanelConstraints);
 		//end Release number
-		
+
 		//Estimate:		
 		//Set the constraints for the "estimateLabel" and add it to the view
 		reqPanelConstraints.fill = GridBagConstraints.HORIZONTAL;
@@ -299,114 +299,8 @@ public class RequirementPanel extends JPanel {
 		add(txtActualEffort, reqPanelConstraints);
 		//end Actual effort
 
-		
-		//Save button:
-		//Set the constraints for the "btnSave" and add it to the view
-		leftConstraints.fill = GridBagConstraints.NONE;//This sets the constraints of this field so that the item will not stretch to fill it's area
-		leftConstraints.weighty = 1;//This is the weight of this field, which tells the layout manager how big this field should be in proportion to the other components
-		leftConstraints.anchor = GridBagConstraints.PAGE_END; //This sets the anchor of the field, here we have told it to anchor the component to the bottom right of it's field
-		leftConstraints.insets = new Insets(10,0,0,0);//Set the top padding to 10 units
-		leftConstraints.gridx = 2;//Set the x coord of the cell of the layout we are describing
-		leftConstraints.gridy = 8;//Set the y coord of the cell of the layout we are describing
-		leftPanel.add(btnSave, leftConstraints);//Actually add the "btnSave" to the layout given the previous constraints
-		//end Save button
-		
-		//end LEFT PANEL
-		
-		
-		//RIGHT PANEL:
-		
-		rightPanel = new JPanel();
-		
-		// Set the layout
-		rightPanel.setLayout(new BorderLayout());
-		
-		// Construct the table model
-		resultsTableModel = new ResultsTableModel();
-		
-		// Construct the table and configure it
-		resultsTable = new JTable(resultsTableModel);
-		resultsTable.setAutoCreateRowSorter(true);
-		resultsTable.setFillsViewportHeight(true);
-		resultsTable.setDefaultRenderer(Date.class, new DateTableCellRenderer());
-		
-		// Add a listener for row clicks
-		//resultsTable.addMouseListener(new RetrieveRequirementController(this));
-		
-		// Put the table in a scroll pane
-		JScrollPane resultsScrollPane = new JScrollPane(resultsTable);
-		resultsScrollPane.setPreferredSize(new Dimension(700,400));
-		
-		btnNew = new JButton("New Requirement");
-		btnRefresh = new JButton("Refresh");
-		
-		//controller = new RetrieveAllRequirementsController(this);
-		btnRefresh.setAction(new RefreshRequirementsAction(controller));
-		
-		// Construct an action listener and add it to the create button
-		btnNew.addActionListener(new ActionListener() {
- 
-            public void actionPerformed(ActionEvent e)
-            {
-            	// set all of the UI fields appropriately when the "create requirement" button is clicked
-            	mode = Mode.CREATE;
-            	
-            	txtName.setText("");
-            	txtDescription.setText("");
-            	typeBox.setSelectedIndex(0);
-            	statusBox.setSelectedIndex(0);
-            	priorityBox.setSelectedIndex(0);
-            	txtReleaseNum.setText("");
-            	txtEstimate.setText("0");
-            	txtActualEffort.setText("0");
-            	
-            	btnSave.setText("Create");
-            	btnSave.setEnabled(true);
-            	
-            	txtName.setEnabled(true);
-            	txtDescription.setEnabled(true);
-            	typeBox.setEnabled(true);
-            	statusBox.setEnabled(false);
-            	priorityBox.setEnabled(true);
-            	txtReleaseNum.setEnabled(true);
-            	txtEstimate.setEnabled(false);
-            	txtActualEffort.setEnabled(false);
-            }
-        });
-		
-		//Actually add the list to the right panel
-		rightPanel.add(resultsScrollPane, BorderLayout.PAGE_START);
-		
-		//Actually add the create button to the right panel
-		rightPanel.add(btnNew, BorderLayout.LINE_START);
-		
-		//Actually add the refresh button to the right panel
-		rightPanel.add(btnRefresh, BorderLayout.LINE_END);
-		
-		//end RIGHT PANEL
-		
-		//MAIN PANEL
-		//Set the layout manager for the main requirement panel
-		setLayout(new GridBagLayout()); //set the layout
-		GridBagConstraints mainConstraints = new GridBagConstraints();//create the constraints variable
-		
-		//Add the two panels to the view
-		mainConstraints.anchor = GridBagConstraints.WEST;
-		add(leftPanel);
-
-		JLabel blankLabel = new JLabel("                 "); //add a blank label to create space between the panels
-		mainConstraints.anchor = GridBagConstraints.CENTER;
-		add(blankLabel);
-		
-		mainConstraints.anchor = GridBagConstraints.EAST;
-		add(rightPanel);
-		
-		
-		//end MAIN PANEL
-		
-
 	}
-	
+
 	/**
 	 * This returns the JTextField "txtName"
 	 * @return the txtName JTextField
@@ -414,7 +308,7 @@ public class RequirementPanel extends JPanel {
 	public JTextField getRequirementName() {
 		return txtName;
 	}
-	
+
 	/**
 	 * This returns the JTextArea "txtDescription"
 	 * @return the txtDescription JTextArea
@@ -422,7 +316,7 @@ public class RequirementPanel extends JPanel {
 	public JTextArea getRequirementDescription() {
 		return txtDescription;
 	}
-	
+
 
 	/**
 	 * This returns the JComboBox "typeBox"
@@ -471,7 +365,7 @@ public class RequirementPanel extends JPanel {
 	public JTextField getRequirementActualEffort() {
 		return txtActualEffort;
 	}
-	
+
 	/**
 	 * This returns the "mode"
 	 * @return the mode
@@ -501,5 +395,5 @@ public class RequirementPanel extends JPanel {
 	public void setCurrentRequirement(Requirement currentRequirement) {
 		this.currentRequirement = currentRequirement;
 	}
-	
+
 }
