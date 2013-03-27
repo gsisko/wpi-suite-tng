52d0c7d810cec9125db1ecfbc4ab1f4835b01c03
clhudson <clhudson@wpi.edu>
2013-03-26 22:40:21 -0400

    Added copyright to list.models and models.validators packages
    Added licensing with all team members to all files in the
    models.validaotrs and list.models packages

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/models/FilterType.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/models/FilterType.java
@@ -1,3 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *		Robert Dabrowski
+ *		Danielle LaRose
+ *		Edison Jimenez
+ *		Christian Gonzalez
+ *		Mike Calder
+ *		John Bosworth
+ *		Paula Rudy
+ *		Gabe Isko
+ *		Bangyan Zhang
+ *		Cassie Hudson
+ *		Robert Smieja
+ *		Alex Solomon
+ *		Brian Hetherman
+ ******************************************************************************/
+
+
 package edu.wpi.cs.wpisuitetng.modules.requirementmanager.list.models;
 
 /**	Possible values for the type of filter.
