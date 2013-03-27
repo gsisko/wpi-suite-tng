86181571d8e2494e3fb0da6a0f46b7c70b5a142a
bangyan zhang <udarshen@gmail.com>
2013-03-27 02:18:32 -0400

    filter builder fixed

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/filter/SaveFilterController.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/filter/SaveFilterController.java
@@ -1,174 +1,174 @@
-/*******************************************************************************
- * Copyright (c) 2013 -- WPI Suite
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *		Robert Dabrowski
- *		Danielle LaRose
- *		Edison Jimenez
- *		Christian Gonzalez
- *		Mike Calder
- *		John Bosworth
- *		Paula Rudy
- *		Gabe Isko
- *		Bangyan Zhang
- *		Cassie Hudson
- *		Robert Smieja
- *		Alex Solomon
- *		Brian Hetherman
- ******************************************************************************/
-
-
-
-package edu.wpi.cs.wpisuitetng.modules.requirementmanager.filter;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JOptionPane;
-
-import edu.wpi.cs.wpisuitetng.modules.requirementmanager.filter.SaveFilterObserver;
-import edu.wpi.cs.wpisuitetng.modules.requirementmanager.list.models.*;
-import edu.wpi.cs.wpisuitetng.modules.requirementmanager.list.views.*;
-import edu.wpi.cs.wpisuitetng.network.Network;
-import edu.wpi.cs.wpisuitetng.network.Request;
-import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
-
-public class SaveFilterController implements ActionListener 
-{
-    private final FilterListPanel panel;
-    private final FilterBuilderPanel builder;
-    private final ListRequirementsView view;
-    
-    public SaveFilterController(ListRequirementsView view) 
-    {
-    	this.panel = view.getListPanel().getFilterPanel();
-    	this.builder = view.getListPanel().getBuilderPanel();
-    	this.view = view;
-    }
-
-
-    public void actionPerformed(ActionEvent event) 
-    {
-	/*
-    	// check if any inputs are invalid, print an error message if one is
-    	if (view.getFilterName().getText().length() == 0) {
-    		JOptionPane.showMessageDialog(null, "Name must be non-blank.", "Error", JOptionPane.ERROR_MESSAGE);
-    		return;
-    	}
-    	if (view.getFilterName().getText().length() > 100) {
-    		JOptionPane.showMessageDialog(null, "Name cannot be greater than 100 characters.", "Error", JOptionPane.ERROR_MESSAGE);
-    		return;
-    	}
-    	if (view.getFilterDescription().getText().length() == 0) {
-    		JOptionPane.showMessageDialog(null, "Description must be non-blank.", "Error", JOptionPane.ERROR_MESSAGE);
-    		return;
-    	}
-    	if (view.getFilterEstimate().getText().length() == 0) {
-    		JOptionPane.showMessageDialog(null, "Estimate must be non-blank.", "Error", JOptionPane.ERROR_MESSAGE);
-    		return;
-    	}
-    	if (view.getFilterActualEffort().getText().length() == 0) {
-    		JOptionPane.showMessageDialog(null, "ActualEffort must be non-blank.", "Error", JOptionPane.ERROR_MESSAGE);
-    		return;
-    	}*/
-
-    	
-	   // if (view.getMode() == CREATE) { // if we are creating a new filter
-		
-	    	// get the fields from the UI
-		FilterType type = FilterType.toType(builder.getFilterType().getSelectedItem().toString());
-		OperatorType comparator = OperatorType.toType(builder.getFilterOperator().toString());
-		String value = builder.getFilterValue().toString();
-		
-			// make a PUT http request and let the observer get the response
-		    final Request request = Network.getInstance().makeRequest("filtermanager/filter", HttpMethod.PUT); // PUT == create
-		    request.setBody(new Filter(type, comparator, value, true).toJSON()); // put the new message in the body of the request
-		    request.addObserver(new SaveFilterObserver(this)); // add an observer to process the response
-		    request.send();
-	    	/*}
-		else { // we are updating an existing filter
-			
-			// make a new filter to store the updated data
-			Filter updatedFilter = new Filter(); 
-
-			Filter oldFilter = builder.;
-
-			
-			// give the new filter the correct ID number
-			updatedFilter.setUniqueID(oldFilter.getUniqueID());
-			
-			updatedFilter.setType(FilterType.toType(builder.getFilterType().toString()));
-			updatedFilter.setComparator(OperatorType.toType(builder.getFilterType().toString()));
-			updatedFilter.setValue(builder.getFilterValue().toString());
-			
-			/*
-			// fill in the rest of the fields with the data from the UI
-			updatedFilter.setName(view.getFilterName().getText());
-			updatedFilter.setDescription(view.getFilterDescription().getText());
-			updatedFilter.setType(FilterType.toType(view.getFilterType().getSelectedItem().toString()));
-			updatedFilter.setReleaseNumber(Integer.parseInt((view.getFilterReleaseNumber().getText().equals("")) ? "-1" : view.getFilterReleaseNumber().getText()));
-			updatedFilter.setStatus(FilterStatus.toStatus(view.getFilterStatus().getSelectedItem().toString()));
-			updatedFilter.setPriority(FilterPriority.toPriority(view.getFilterPriority().getSelectedItem().toString()));
-			updatedFilter.setEstimate(Integer.parseInt(view.getFilterEstimate().getText()));
-			updatedFilter.setActualEffort(Integer.parseInt(view.getFilterActualEffort().getText()));
-			*/
-			// make a POST http request and let the observer get the response
-		    final Request request = Network.getInstance().makeRequest("filtermanager/filter", HttpMethod.POST); // POST == update
-		    request.setBody(updatedFilter.toJSON()); // put the new message in the body of the request
-		    request.addObserver(new SaveFilterObserver(this)); // add an observer to process the response
-		    request.send();
-			
-
-		}
-
-	
-    }
-    
-    
-    /**
-     * Simple success message for saving a new filter.  If we want the boxes to clear automatically,
-     * this is probably where we would want to implement it.
-     * @param newReq Filter that was saved.
-     */
-	public void saveSuccess(Filter newReq) {
-		// if success, set all of the UI fields appropriately for post-save actions
-		if (newReq != null) {
-			System.out.print("Filter " +/* newReq.getId() +*/ " saved successfully\n");
-			/*
-			view.getFilterName().setText("");
-			view.getFilterDescription().setText("");
-			view.getFilterType().setSelectedIndex(0);
-			view.getFilterStatus().setSelectedIndex(0);
-			view.getFilterPriority().setSelectedIndex(0);
-			view.getFilterReleaseNumber().setText("");
-			view.getFilterEstimate().setText("");
-			view.getFilterActualEffort().setText("");
-			
-			view.getSaveButton().setText("Update");
-			view.getSaveButton().setEnabled(false);
-
-			view.getFilterName().setEnabled(false);
-			view.getFilterDescription().setEnabled(false);
-			view.getFilterType().setEnabled(false);
-			view.getFilterStatus().setEnabled(false);
-			view.getFilterPriority().setEnabled(false);
-			view.getFilterReleaseNumber().setEnabled(false);
-			view.getFilterEstimate().setEnabled(false);
-			view.getFilterActualEffort().setEnabled(false);*/
-		}
-		else {
-			System.err.print("Undected error saving filter\n");
-		}
-	}
-	
-	
-	public ListRequirementsView getView() {
-		return view;
-	}
-}
-
+///*******************************************************************************
+// * Copyright (c) 2013 -- WPI Suite
+// *
+// * All rights reserved. This program and the accompanying materials
+// * are made available under the terms of the Eclipse Public License v1.0
+// * which accompanies this distribution, and is available at
+// * http://www.eclipse.org/legal/epl-v10.html
+// *
+// * Contributors:
+// *		Robert Dabrowski
+// *		Danielle LaRose
+// *		Edison Jimenez
+// *		Christian Gonzalez
+// *		Mike Calder
+// *		John Bosworth
+// *		Paula Rudy
+// *		Gabe Isko
+// *		Bangyan Zhang
+// *		Cassie Hudson
+// *		Robert Smieja
+// *		Alex Solomon
+// *		Brian Hetherman
+// ******************************************************************************/
+//
+//
+//
+//package edu.wpi.cs.wpisuitetng.modules.requirementmanager.filter;
+//
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//
+//import javax.swing.JOptionPane;
+//
+//import edu.wpi.cs.wpisuitetng.modules.requirementmanager.filter.SaveFilterObserver;
+//import edu.wpi.cs.wpisuitetng.modules.requirementmanager.list.models.*;
+//import edu.wpi.cs.wpisuitetng.modules.requirementmanager.list.views.*;
+//import edu.wpi.cs.wpisuitetng.network.Network;
+//import edu.wpi.cs.wpisuitetng.network.Request;
+//import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
+//
+//public class SaveFilterController implements ActionListener 
+//{
+//    private final FilterListPanel panel;
+//    private final FilterBuilderPanel builder;
+//    private final ListRequirementsView view;
+//    
+//    public SaveFilterController(ListRequirementsView view) 
+//    {
+//    	this.panel = view.getListPanel().getFilterPanel();
+//    	this.builder = view.getListPanel().getBuilderPanel();
+//    	this.view = view;
+//    }
+//
+//
+//    public void actionPerformed(ActionEvent event) 
+//    {
+//	/*
+//    	// check if any inputs are invalid, print an error message if one is
+//    	if (view.getFilterName().getText().length() == 0) {
+//    		JOptionPane.showMessageDialog(null, "Name must be non-blank.", "Error", JOptionPane.ERROR_MESSAGE);
+//    		return;
+//    	}
+//    	if (view.getFilterName().getText().length() > 100) {
+//    		JOptionPane.showMessageDialog(null, "Name cannot be greater than 100 characters.", "Error", JOptionPane.ERROR_MESSAGE);
+//    		return;
+//    	}
+//    	if (view.getFilterDescription().getText().length() == 0) {
+//    		JOptionPane.showMessageDialog(null, "Description must be non-blank.", "Error", JOptionPane.ERROR_MESSAGE);
+//    		return;
+//    	}
+//    	if (view.getFilterEstimate().getText().length() == 0) {
+//    		JOptionPane.showMessageDialog(null, "Estimate must be non-blank.", "Error", JOptionPane.ERROR_MESSAGE);
+//    		return;
+//    	}
+//    	if (view.getFilterActualEffort().getText().length() == 0) {
+//    		JOptionPane.showMessageDialog(null, "ActualEffort must be non-blank.", "Error", JOptionPane.ERROR_MESSAGE);
+//    		return;
+//    	}*/
+//
+//    	
+//	   // if (view.getMode() == CREATE) { // if we are creating a new filter
+//		
+//	    	// get the fields from the UI
+//		FilterType type = FilterType.toType(builder.getFilterType().getSelectedItem().toString());
+//		OperatorType comparator = OperatorType.toType(builder.getFilterOperator().toString());
+//		String value = builder.getFilterValue().toString();
+//		
+//			// make a PUT http request and let the observer get the response
+//		    final Request request = Network.getInstance().makeRequest("filtermanager/filter", HttpMethod.PUT); // PUT == create
+//		    request.setBody(new Filter(type, comparator, value, true).toJSON()); // put the new message in the body of the request
+//		    request.addObserver(new SaveFilterObserver(this)); // add an observer to process the response
+//		    request.send();
+//	    	/*}
+//		else { // we are updating an existing filter
+//			
+//			// make a new filter to store the updated data
+//			Filter updatedFilter = new Filter(); 
+//
+//			Filter oldFilter = builder.;
+//
+//			
+//			// give the new filter the correct ID number
+//			updatedFilter.setUniqueID(oldFilter.getUniqueID());
+//			
+//			updatedFilter.setType(FilterType.toType(builder.getFilterType().toString()));
+//			updatedFilter.setComparator(OperatorType.toType(builder.getFilterType().toString()));
+//			updatedFilter.setValue(builder.getFilterValue().toString());
+//			
+//			/*
+//			// fill in the rest of the fields with the data from the UI
+//			updatedFilter.setName(view.getFilterName().getText());
+//			updatedFilter.setDescription(view.getFilterDescription().getText());
+//			updatedFilter.setType(FilterType.toType(view.getFilterType().getSelectedItem().toString()));
+//			updatedFilter.setReleaseNumber(Integer.parseInt((view.getFilterReleaseNumber().getText().equals("")) ? "-1" : view.getFilterReleaseNumber().getText()));
+//			updatedFilter.setStatus(FilterStatus.toStatus(view.getFilterStatus().getSelectedItem().toString()));
+//			updatedFilter.setPriority(FilterPriority.toPriority(view.getFilterPriority().getSelectedItem().toString()));
+//			updatedFilter.setEstimate(Integer.parseInt(view.getFilterEstimate().getText()));
+//			updatedFilter.setActualEffort(Integer.parseInt(view.getFilterActualEffort().getText()));
+//			*/
+//			// make a POST http request and let the observer get the response
+//		    final Request request = Network.getInstance().makeRequest("filtermanager/filter", HttpMethod.POST); // POST == update
+//		    request.setBody(updatedFilter.toJSON()); // put the new message in the body of the request
+//		    request.addObserver(new SaveFilterObserver(this)); // add an observer to process the response
+//		    request.send();
+//			
+//
+//		}
+//
+//	
+//    }
+//    
+//    
+//    /**
+//     * Simple success message for saving a new filter.  If we want the boxes to clear automatically,
+//     * this is probably where we would want to implement it.
+//     * @param newReq Filter that was saved.
+//     */
+//	public void saveSuccess(Filter newReq) {
+//		// if success, set all of the UI fields appropriately for post-save actions
+//		if (newReq != null) {
+//			System.out.print("Filter " +/* newReq.getId() +*/ " saved successfully\n");
+//			/*
+//			view.getFilterName().setText("");
+//			view.getFilterDescription().setText("");
+//			view.getFilterType().setSelectedIndex(0);
+//			view.getFilterStatus().setSelectedIndex(0);
+//			view.getFilterPriority().setSelectedIndex(0);
+//			view.getFilterReleaseNumber().setText("");
+//			view.getFilterEstimate().setText("");
+//			view.getFilterActualEffort().setText("");
+//			
+//			view.getSaveButton().setText("Update");
+//			view.getSaveButton().setEnabled(false);
+//
+//			view.getFilterName().setEnabled(false);
+//			view.getFilterDescription().setEnabled(false);
+//			view.getFilterType().setEnabled(false);
+//			view.getFilterStatus().setEnabled(false);
+//			view.getFilterPriority().setEnabled(false);
+//			view.getFilterReleaseNumber().setEnabled(false);
+//			view.getFilterEstimate().setEnabled(false);
+//			view.getFilterActualEffort().setEnabled(false);*/
+//		}
+//		else {
+//			System.err.print("Undected error saving filter\n");
+//		}
+//	}
+//	
+//	
+//	public ListRequirementsView getView() {
+//		return view;
+//	}
+//}
+//
--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/views/FilterBuilderPanel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/views/FilterBuilderPanel.java
@@ -1,4 +1,3 @@
-
 /*******************************************************************************
  * Copyright (c) 2013 -- WPI Suite
  *
@@ -54,7 +53,7 @@ import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.list.models.Filter;
 
 /**
- * Panel to contain the filter builder for requirement searching
+ * Panel to contain the filter builder for defect searching
  */
 @SuppressWarnings("serial")
 public class FilterBuilderPanel extends JPanel {
@@ -64,7 +63,7 @@ public class FilterBuilderPanel extends JPanel {
 		CREATE,
 		EDIT
 	}
-	
+
 	//the labels
 	private final JLabel typeLabel; 
 	private final JLabel comparatorLabel;
@@ -76,11 +75,12 @@ public class FilterBuilderPanel extends JPanel {
 	private final JComboBox<String> comparatorBox;
 	private final JTextField txtValue;
 	private final JComboBox<String> userFilterBox;
+	private Filter currentFilter;
+	private Mode mode;
 
-	
 	//button
 	private final JButton Save;
-	
+
 	private final ListPanel parent;
 	/**
 	 * Construct the panel
@@ -88,11 +88,11 @@ public class FilterBuilderPanel extends JPanel {
 	@SuppressWarnings("deprecation")
 	public FilterBuilderPanel(ListPanel view) {
 		parent = view;
-		
+		mode = Mode.CREATE;
 		//create title
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		this.setBorder(BorderFactory.createTitledBorder("Filter Builder"));
-		
+
 		//construct the panels
 		typeLabel = new JLabel("Type:");
 		comparatorLabel = new JLabel("Comparator:");
@@ -103,19 +103,19 @@ public class FilterBuilderPanel extends JPanel {
 		//construct the components
 		txtValue = new JTextField();
 		txtValue.setEnabled(true);
-		
-		
+
+
 
 		//create strings for the boxes
 		String[] typeStrings = { "Id", "Name", "Description","Type", "Status","Priority","ReleaseNumber","Estimate","ActualEffort"};
 		String[] comparatorStrings = {"Greater than","GreaterThanOrEqualTo","LessThan","LessThanOrEqualTo","EqualTo","NotEqualTo","Contains","DoesNotContain"};
 		String[] userFilterStrings ={"Active","Inactive"};
-		
+
 		//construct the boxes
 		typeBox = new JComboBox<String>(typeStrings);
 		comparatorBox = new JComboBox<String>(comparatorStrings);
 		userFilterBox = new JComboBox<String>(userFilterStrings);
-		
+
 		//set initial conditions
 		typeBox.setSelectedIndex(0);
 		typeBox.setEnabled(true);
@@ -130,7 +130,7 @@ public class FilterBuilderPanel extends JPanel {
 		//set the layout
 		setLayout(new GridBagLayout());
 		GridBagConstraints FilterBuilderConstraints = new GridBagConstraints();
-		FilterBuilderConstraints.anchor= GridBagConstraints.CENTER;
+		FilterBuilderConstraints.anchor= GridBagConstraints.NORTH;
 
 		//adjust location
 		Save.setAlignmentX(Component.RIGHT_ALIGNMENT);
@@ -140,21 +140,14 @@ public class FilterBuilderPanel extends JPanel {
 		FilterBuilderConstraints.fill = GridBagConstraints.HORIZONTAL;//This sets the constraints of this field so that the item will stretch horizontally to fill it's area
 		FilterBuilderConstraints.ipady = 0;//This tells the layout to reset the vertical ipad from the previously defined 20 units to now 0 units
 		FilterBuilderConstraints.anchor = GridBagConstraints.FIRST_LINE_END; //This sets the anchor of the field, here we have told it to anchor the component to the top center of it's field
-
-		//FilterBuilderConstraints.insets = new Insets(10,0,0,0);  //Set the top padding to 10 units  of blank space
+		FilterBuilderConstraints.insets = new Insets(0,25,0,0);  //Set the top padding to 10 units  of blank space
 		FilterBuilderConstraints.gridx = 0;//Set the x coord of the cell of the layout we are describing
-		FilterBuilderConstraints.gridy = 3;//Set the y coord of the cell of the layout we are describing
-		FilterBuilderConstraints.ipadx=15;
-		add(typeLabel, FilterBuilderConstraints);//Actually add the "descriptionLabel" to the layout given the previous constraints
-
+		FilterBuilderConstraints.gridy = 1;//Set the y coord of the cell of the layout we are describing
+		add(typeLabel, FilterBuilderConstraints);//Actually add the "typenLabel" to the layout given the previous constraints
 		//Set the constraints for the "typeBox"  and add it to the view
 		FilterBuilderConstraints.fill = GridBagConstraints.FIRST_LINE_START;//This sets the constraints of this field so that the item will stretch both horizontally and vertically to fill it's area
 		FilterBuilderConstraints.gridx = 1;//Set the x coord of the cell of the layout we are describing
-
-	//	FilterBuilderConstraints.gridwidth = 1;   //Tell the layout that this field will fill 2 columns
-		FilterBuilderConstraints.gridy = 3;//Set the y coord of the cell of the layout we are describing
-		//FilterBuilderConstraints.ipadx=15;
-
+		FilterBuilderConstraints.gridy = 1;//Set the y coord of the cell of the layout we are describing
 		add(typeBox, FilterBuilderConstraints);//Actually add the "typeBox" to the layout given the previous constraints
 		//end Type
 
@@ -163,21 +156,14 @@ public class FilterBuilderPanel extends JPanel {
 		FilterBuilderConstraints.fill = GridBagConstraints.HORIZONTAL;//This sets the constraints of this field so that the item will stretch horizontally to fill it's area
 		FilterBuilderConstraints.ipady = 0;//This tell	s the layout to reset the vertical ipad from the previously defined 20 units to now 0 units
 		FilterBuilderConstraints.anchor = GridBagConstraints.FIRST_LINE_END; //This sets the anchor of the field, here we have told it to anchor the component to the top center of it's field
-
-		//FilterBuilderConstraints.insets = new Insets(10,0,0,0);  //Set the top padding to 10 units  of blank space
+		FilterBuilderConstraints.insets = new Insets(0,25,0,0);
 		FilterBuilderConstraints.gridx = 2;//Set the x coord of the cell of the layout we are describing
-		FilterBuilderConstraints.gridy = 3;//Set the y coord of the cell of the layout we are describing
-		FilterBuilderConstraints.ipadx=15;
-		add(comparatorLabel, FilterBuilderConstraints);//Actually add the "descriptionLabel" to the layout given the previous constraints
-
+		FilterBuilderConstraints.gridy = 1;//Set the y coord of the cell of the layout we are describing
+		add(comparatorLabel, FilterBuilderConstraints);//Actually add the "comparatorLabel" to the layout given the previous constraints
 		//Set the constraints for the "comparator"  and add it to the view
 		FilterBuilderConstraints.fill = GridBagConstraints.FIRST_LINE_START;//This sets the constraints of this field so that the item will stretch both horizontally and vertically to fill it's area
 		FilterBuilderConstraints.gridx = 3;//Set the x coord of the cell of the layout we are describing
-
-	//	FilterBuilderConstraints.gridwidth = 2;   //Tell the layout that this field will fill 2 columns
-		FilterBuilderConstraints.gridy = 3;//Set the y coord of the cell of the layout we are describing
-		//FilterBuilderConstraints.ipadx=15;
-
+		FilterBuilderConstraints.gridy = 1;//Set the y coord of the cell of the layout we are describing
 		add(comparatorBox, FilterBuilderConstraints);//Actually add the "comparatorBox" to the layout given the previous constraints
 		//end comparator
 
@@ -186,21 +172,14 @@ public class FilterBuilderPanel extends JPanel {
 		FilterBuilderConstraints.fill = GridBagConstraints.HORIZONTAL;//This sets the constraints of this field so that the item will stretch horizontally to fill it's area
 		FilterBuilderConstraints.ipady = 0;//This tells the layout to reset the vertical ipad from the previously defined 20 units to now 0 units
 		FilterBuilderConstraints.anchor = GridBagConstraints.FIRST_LINE_START; //This sets the anchor of the field, here we have told it to anchor the component to the top center of it's field
-
-		//FilterBuilderConstraints.insets = new Insets(10,0,0,0);  //Set the top padding to 10 units  of blank space
+		FilterBuilderConstraints.insets = new Insets(0,25,0,0);
 		FilterBuilderConstraints.gridx = 4;//Set the x coord of the cell of the layout we are describing
-		FilterBuilderConstraints.gridy = 3;//Set the y coord of the cell of the layout we are describing
-		FilterBuilderConstraints.ipadx=15;
-		add(userFilterLabel, FilterBuilderConstraints);//Actually add the "descriptionLabel" to the layout given the previous constraints
-
+		FilterBuilderConstraints.gridy = 1;//Set the y coord of the cell of the layout we are describing
+		add(userFilterLabel, FilterBuilderConstraints);//Actually add the "userFilterLabel" to the layout given the previous constraints
 		//Set the constraints for the "userfilter"  and add it to the view
 		FilterBuilderConstraints.fill = GridBagConstraints.FIRST_LINE_START;//This sets the constraints of this field so that the item will stretch both horizontally and vertically to fill it's area
 		FilterBuilderConstraints.gridx = 5;//Set the x coord of the cell of the layout we are describing
-
-	//	FilterBuilderConstraints.gridwidth = 2;   //Tell the layout that this field will fill 2 columns
-		FilterBuilderConstraints.gridy = 3;//Set the y coord of the cell of the layout we are describing
-		//FilterBuilderConstraints.ipadx=15;
-
+		FilterBuilderConstraints.gridy = 1;//Set the y coord of the cell of the layout we are describing
 		add(userFilterBox, FilterBuilderConstraints);//Actually add the "userFilterBox" to the layout given the previous constraints
 		//end userfilter
 
@@ -208,34 +187,25 @@ public class FilterBuilderPanel extends JPanel {
 		//Set the constraints for the "value" and add it to the view
 		FilterBuilderConstraints.fill = GridBagConstraints.HORIZONTAL;//This sets the constraints of this field so that the item will stretch horizontally to fill it's area
 		FilterBuilderConstraints.anchor = GridBagConstraints.FIRST_LINE_START; //This sets the anchor of the field, here we have told it to anchor the component to the top center of it's field
-
-		//FilterBuilderConstraints.insets = new Insets(10,0,0,0);  //Set the top padding to 10 units  of blank space
+		FilterBuilderConstraints.insets = new Insets(0,25,0,0);
 		FilterBuilderConstraints.gridx = 6;//Set the x coord of the cell of the layout we are describing
-		FilterBuilderConstraints.gridy = 3;//Set the y coord of the cell of the layout we are describing
-		FilterBuilderConstraints.ipadx=15;
-		add(valueLabel, FilterBuilderConstraints);//Actually add the "releaseNumLabel" to the layout given the previous constraints
-
+		FilterBuilderConstraints.gridy = 1;//Set the y coord of the cell of the layout we are describing
+		add(valueLabel, FilterBuilderConstraints);//Actually add the "valueLabel" to the layout given the previous constraints
 		//Set the constraints for the "value" and add it to the view
 		FilterBuilderConstraints.fill = GridBagConstraints.FIRST_LINE_START;//This sets the constraints of this field so that the item will stretch horizontally to fill it's area
 		FilterBuilderConstraints.gridx = 7;//Set the x coord of the cell of the layout we are describing
-
-	//	FilterBuilderConstraints.gridwidth = 2;   //Tell the layout that this field will fill 2 columns
-		FilterBuilderConstraints.gridy = 3;//Set the y coord of the cell of the layout we are describing
-
+		FilterBuilderConstraints.gridy = 1;//Set the y coord of the cell of the layout we are describing
 		FilterBuilderConstraints.ipadx=80;
 		add(txtValue, FilterBuilderConstraints);//Actually add the "txtValue" to the layout given the previous constraints
 		//end value
-		
+
 		//Save button:
 		//Set the constraints for the "Save" and add it to the view
 		FilterBuilderConstraints.fill = GridBagConstraints.NONE;//This sets the constraints of this field so that the item will not stretch to fill it's area
-
-		FilterBuilderConstraints.weighty = 1;//This is the weight of this field, which tells the layout manager how big this field should be in proportion to the other components
-		FilterBuilderConstraints.anchor = GridBagConstraints.EAST; //This sets the anchor of the field, here we have told it to anchor the component to the bottom right of it's field
-		//FilterBuilderConstraints.insets = new Insets(10,0,0,0);//Set the top padding to 10 units
-		FilterBuilderConstraints.gridx = 7;//Set the x coord of the cell of the layout we are describing
-		FilterBuilderConstraints.gridy = 4;//Set the y coord of the cell of the layout we are describing
-
+		FilterBuilderConstraints.anchor = GridBagConstraints.FIRST_LINE_START; //This sets the anchor of the field, here we have told it to anchor the component to the bottom right of it's field
+		FilterBuilderConstraints.gridx = 8;//Set the x coord of the cell of the layout we are describing
+		FilterBuilderConstraints.gridy = 1;//Set the y coord of the cell of the layout we are describing
+		Save.setPreferredSize(new Dimension (15,20));
 		add(Save, FilterBuilderConstraints);//Actually add the "Save" to the layout given the previous constraints
 		//end Save button
 
@@ -245,22 +215,40 @@ public class FilterBuilderPanel extends JPanel {
 	{
 	    return typeBox;
 	}
-	
+
 	public JComboBox<String> getFilterOperator()
 	{
 	    return comparatorBox;
 	}
-	
+
 	public JTextField getFilterValue()
 	{
 	    return txtValue;
 	}
-	
+
 	public JComboBox<String> getStatus() //?
 	{
 	    return userFilterBox;
 	}
 
+	public Mode getMode()
+	{
+	    return mode;
+	}
 
+	public void setMode(Mode editMode)
+	{
+	    mode = editMode;
+	}
+
+	public Filter getCurrentFilter()
+	{
+	    return currentFilter;
+	}
+
+	public void setCurrentFilter(Filter newFilter)
+	{
+	    currentFilter = newFilter;
+	}
 
 }
