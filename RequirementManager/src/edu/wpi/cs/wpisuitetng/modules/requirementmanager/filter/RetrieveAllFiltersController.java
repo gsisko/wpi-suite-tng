cbb3fa11219f6c8d8c6535d1e450cf286ac88c61
Mike Calder <Mike@Mike-Calders-MacBook-Pro.local>
2013-03-28 01:40:19 -0400

    Renaming Operator Column

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/filter/RetrieveAllFiltersController.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/filter/RetrieveAllFiltersController.java
@@ -96,7 +96,7 @@ public class RetrieveAllFiltersController {
 			this.data = filters;
 			
 			// set the column names
-			String[] columnNames = {"Id", "Type", "Comparator", "Value", "Active"};
+			String[] columnNames = {"Id", "Type", "Op", "Value", "Active"};
 			
 			// put the data in the table
 			Object[][] entries = new Object[filters.length][columnNames.length];
--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/views/FilterBuilderPanel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/views/FilterBuilderPanel.java
@@ -89,7 +89,7 @@ public class FilterBuilderPanel extends JPanel implements ActionListener{
 
 		//construct the panels
 		typeLabel = new JLabel("Type:");
-		comparatorLabel = new JLabel("Comparator:");
+		comparatorLabel = new JLabel("Operator:");
 		valueLabel = new JLabel("Value:");
 		btnSave= new JButton("Create");
 
