a2e23ee7248e4bbf7b94268ab0df23407cce26f1
clhudson <clhudson@wpi.edu>
2013-03-30 14:19:53 -0400

    Server side of Notes implemented
    Added the server side for Notes

---
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/models/Note.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *		Robert Dabrowski
+ *		Danielle LaRose
+ *		Edison Jimenez
+ *		Christian Gonzalez
+ *		Mike Calder
+ *		John Bosworth
+ *		Paula Rudy
+ *		Gabe Isko
+ *		Bangyan Zhang
+ *		Cassie Hudson
+ *		Robert Smieja
+ *		Alex Solomon
+ *		Brian Hetherman
+ ******************************************************************************/
+
+package edu.wpi.cs.wpisuitetng.modules.requirementmanager.models;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+
+public class Note {
+	private String message;
+	private User user; 
+	private Date date;
+	
+	/**
+	 * Create a Note with given properties
+	 * 
+	 * @param user the User who created the Note
+	 * @param message the User wishes to post
+	 * @param date the timestamp of the posted Note
+	 */
+	public Note(String message)
+	{
+		this.setMessage(message);
+		this.setUser(new User("","","",-1));
+		this.date = new Date();
+	}
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+}
