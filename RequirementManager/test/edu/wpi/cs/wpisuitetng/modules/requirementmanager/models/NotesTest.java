c346e94ad174be6124bedcfeffaf9899e03790a8
Christian Gonzalez <cpgonzalez@wpi.edu>
2013-03-30 15:47:12 -0400

    More tests on notes; organized and commented

--- RequirementManager/test/edu/wpi/cs/wpisuitetng/modules/requirementmanager/models/NotesTest.java
+++ RequirementManager/test/edu/wpi/cs/wpisuitetng/modules/requirementmanager/models/NotesTest.java
@@ -12,6 +12,7 @@ public class NotesTest {
 	
 	@Before
 	public void setUp() throws Exception{
+		//seting up new requirement
 		r1 = new Requirement("Tester Requirement", "Testing Description", RequirementType.Theme, RequirementPriority.NoPriority, 0);
 		r1.setPriority(RequirementPriority.Medium);
 		r1.setReleaseNumber(8000);
@@ -19,36 +20,44 @@ public class NotesTest {
 		r1.setActualEffort(18);
 		r1.setStatus(RequirementStatus.InProgress);
 		r1.setId(50);
-		
+		//set first note to index 0
 		r1.getNotes().add(new Note("I am testing notes."));
 		n1 = r1.getNotes().get(r1.getNotes().size() - 1);
+		// adding multiple notes
+		r1.getNotes().add(new Note("!@#$#$%^*&**()")); // note at index 1
+		r1.getNotes().add(new Note("ASADSFBassfkh")); // note at index 2
+		r1.getNotes().add(new Note("1234abcd")); // note at index 3
+		r1.getNotes().add(new Note("No ho fo lo bro")); // note at index 4
 	}
 	
 	@Test
 
 	public void testGetMessage(){
+		// does getMessage work?
 		assertEquals(n1.getMessage(), "I am testing notes.");
 	}
 	
 	@Test
 	public void testSetMessage(){
+		// does setMessage work?
 		n1.setMessage("Still testing");
 		assertEquals(n1.getMessage(), "Still testing");
 	}
 	
-	/**
 	@Test
 	public void testMultipleNotes(){
-		r1.getNotes().get(r1.getNotes().size() - 1);
-		r1.getMessage()
+		// did the multiple notes get added correctly?
+		assertEquals(r1.getNotes().get(1).getMessage(), "!@#$#$%^*&**()");
+		assertEquals(r1.getNotes().get(2).getMessage(), "ASADSFBassfkh");
+		assertEquals(r1.getNotes().get(3).getMessage(), "1234abcd");
+		assertEquals(r1.getNotes().get(4).getMessage(), "No ho fo lo bro");
 	}
-	
-	*/
 	
 	@Test
 	public void testfromJSON() {
 		String json = r1.toJSON();
 		Requirement newRequirement = Requirement.fromJSON(json);
+		// did editing and altering notes affect other elements in requirement?
 		assertEquals(newRequirement.getName(), "Tester Requirement");
 		assertEquals(newRequirement.getPriority(), RequirementPriority.Medium);
 		assertEquals(newRequirement.getDescription(), "Testing Description");
@@ -58,6 +67,7 @@ public class NotesTest {
 		assertEquals(newRequirement.getActualEffort(), 18);
 		assertEquals(newRequirement.getStatus(), RequirementStatus.InProgress);
 		assertEquals(newRequirement.getId(), 50);
+		assertEquals(newRequirement.getNotes(), r1.getNotes());
 	}
 
 }
