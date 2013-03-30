e4cce188424c18432898de10d576c6a2cd61590b
Gabe Isko <gabestripes@gmail.com>
2013-03-30 14:44:15 -0400

    add create note action listener

---
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/requirement/SaveNoteAction.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *		Robert Dabrowski
+ *		Danielle LaRose
+ *		Edison Jimenez
+ *		Christian Gonzalez
+ *		Mike Calder
+ *		John Bosworth
+ *		Paula Rudy
+ *		Gabe Isko
+ *		Bangyan Zhang
+ *		Cassie Hudson
+ *		Robert Smieja
+ *		Alex Solomon
+ *		Brian Hetherman
+ ******************************************************************************/
+
+package edu.wpi.cs.wpisuitetng.modules.requirementmanager.requirement;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+
+/**
+ * Action that calls {@link SaveRequirementController#save}, default mnemonic key is S.
+ */
+@SuppressWarnings("serial")
+public class SaveNoteAction extends AbstractAction {
+
+	private final SaveNoteController controller;
+	
+	/**
+	 * Create a SaveChangesAction
+	 * @param controller When the action is performed, controller.save will be called
+	 */
+	public SaveNoteAction(SaveNoteController controller) {
+		super("Save Note");
+		this.controller = controller;
+		putValue(MNEMONIC_KEY, KeyEvent.VK_S);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		controller.save();
+	}
+
+}
