142bbfa04b388ff898ed8412fe61cdfcff0a0ef1
clhudson <clhudson@wpi.edu>
2013-04-23 22:16:50 -0400

    Changed License to Team 5 D13

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/models/Requirement.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/models/Requirement.java
@@ -7,20 +7,8 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *		Robert Dabrowski
- *		Danielle LaRose
- *		Edison Jimenez
- *		Christian Gonzalez
- *		Mike Calder
- *		John Bosworth
- *		Paula Rudy
- *		Gabe Isko
- *		Bangyan Zhang
- *		Cassie Hudson
- *		Robert Smieja
- *		Alex Solomon
- *		Brian Hetherman
+ * Contributors: Team 5 D13
+ * 
  ******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.requirementmanager.models;
--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/requirement/JTextFieldLimit.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/requirement/JTextFieldLimit.java
@@ -1,7 +1,16 @@
-/**
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Team 5 D13
+ * 
  * Code sourced from:
  * http://www.java2s.com/Code/Java/Swing-JFC/LimitJTextFieldinputtoamaximumlength.htm
- */
+ ******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.requirementmanager.requirement;
 
