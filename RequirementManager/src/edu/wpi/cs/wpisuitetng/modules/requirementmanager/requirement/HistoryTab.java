bbf3e21f5450e7456db367a6a1992fb62a5f8d9e
Paula Rudy <paularudy@gmail.com>
2013-04-26 12:42:25 -0400

    Oh. My. Glob. MORE COMMENT UPDATES

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/requirement/HistoryTab.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/requirement/HistoryTab.java
@@ -20,27 +20,31 @@ import javax.swing.JScrollPane;
 
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.changeset.RequirementEvent;
 
-
+/** This class is a JPanel that holds the GUI for displaying the history of a requirement.
+ * It is kept inside the RequirementTabPanel JTabbedPane within the RequirementTab JPanel.
+ * It contains a ListOfEventPanel ("eventList"), in a JScrollPane ("listScrollPane"), that
+ * holds all the EventPanels (containing all the events associated with this requirement)
+ * to display.
+ */
 @SuppressWarnings({"serial"})
 public class HistoryTab extends JPanel {
 
-	// The parent 
-	private RequirementTab parent; //Stores the RequirementPanel that contains the panel
+	/** The parent RequirementTab - this stores a reference to the RequirementPanel that contains this HistoryTab panel (within the RequirementTabPanel) */
+	private RequirementTab parent;
 
-	//The panel to hold all the NotePanels (containing all the notes) to display
+	/** The panel to hold all the EventPanels (containing all the events) to display */
 	private ListOfEventPanel eventList;
 
-	//A scroll pane to hold the "noteList"
+	/** A scroll pane to hold the "eventList" */
 	JScrollPane listScrollPane;
 
-	/**
-	 * The constructor for NotePanel;
+	/** The constructor for HistoryTab;
 	 * Construct the panel, the components, and add the
 	 * components to the panel.
-	 * @param reqPanelParent	The parent of this tab
+	 * 
+	 * @param reqPanelParent	The RequirementTab parent of this tab
 	 */
 	public HistoryTab(RequirementTab reqPanelParent) {
-
 		parent = reqPanelParent; //Set the RequirementPanel that contains this instance of this panel
 
 		// Create and set the layout manager
@@ -62,34 +66,43 @@ public class HistoryTab extends JPanel {
 		this.add(listScrollPane); //add the noteList, in the listScrollPane, to the panel
 	}
 
+	/**
+	 * @return eventList The ListOfEventPanel that holds all the EventPanels (containing all the events) to display in this HistoryTab
+	 */
 	public ListOfEventPanel getEventList() {
 		return eventList;
 	}
 
+	/** This method refreshes the eventList (the ListOfEventPanel that holds all the EventPanels, 
+	 *  containing all the events, to display in this HistoryTab). This is used because a
+	 *  ListOfEventPanel must be recreated to show changes in the list of events associated 
+	 *  with this requirement.
+	 */
 	public void refreshEventsList() {
-		this.removeAll();
-		eventList  = new ListOfEventPanel(parent.getCurrentRequirement());
-
-		listScrollPane = new JScrollPane(eventList);
-
-		listScrollPane.setPreferredSize(new Dimension(580, 300));		
-
-		// Add components
-		this.add(listScrollPane); //add the noteList, in the listScrollPane, to the panel
+		this.removeAll();//Remove all the components from this HistoryTab panel
+		eventList  = new ListOfEventPanel(parent.getCurrentRequirement()); //Create a new ListOfEventPanel
+		listScrollPane = new JScrollPane(eventList);// Put the new ListOfEventPanel in the scroll pane
+		listScrollPane.setPreferredSize(new Dimension(580, 300));//Re-set the preferred size so that the scroll and resize functionality will function properly
+		this.add(listScrollPane); // Re-add the new components; add the noteList, in the listScrollPane, to the panel
 	}
 
+	/** This method, like {@link edu.wpi.cs.wpisuitetng.modules.requirementmanager.requirement#refreshEventsList()},
+	 *  refreshes the eventList (the ListOfEventPanel that holds all the EventPanels, containing all the events,
+	 *  to display in this HistoryTab).
+	 *  Unlike refreshEventsList, however, this method also allows you to specifically add a new 
+	 *  Event to the list of events associated with this requirement. 
+	 *  This method, like refreshEventsList, is used because a ListOfEventPanel must be recreated
+	 *  to show changes in the list of events associated with this requirement.
+	 * 
+	 * @param newEvent The RequirementEvent to add to the list of events associated with this requirement.
+	 */
 	public void addEventToList(RequirementEvent newEvent){
-		this.removeAll();
-		parent.getCurrentRequirement().getEvents().add(newEvent);
-		eventList  = new ListOfEventPanel(parent.getCurrentRequirement());
-
-		listScrollPane = new JScrollPane(eventList);
-
-		listScrollPane.setPreferredSize(new Dimension(580, 300));		
-
-		// Add components
-		this.add(listScrollPane); //add the noteList, in the listScrollPane, to the panel
-
+		this.removeAll();//Remove all the components from this HistoryTab panel
+		parent.getCurrentRequirement().getEvents().add(newEvent);//Add the event, passed in as "newEvent", to the list of events associated with this requirement
+		eventList  = new ListOfEventPanel(parent.getCurrentRequirement()); //Create a new ListOfEventPanel
+		listScrollPane = new JScrollPane(eventList);// Put the new ListOfEventPanel in the scroll pane
+		listScrollPane.setPreferredSize(new Dimension(580, 300));//Re-set the preferred size so that the scroll and resize functionality will function properly		
+		this.add(listScrollPane); // Re-add the new components; add the noteList, in the listScrollPane, to the panel
 	}
 
 }
--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/requirement/ListOfEventPanel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/requirement/ListOfEventPanel.java
@@ -26,15 +26,27 @@ import javax.swing.JPanel;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.changeset.RequirementEvent;
 
-
+/** This panel contains a list of events associated with a particular Requirement,
+ *  each event in it's own EventPanel.
+ *  Please note that the ListOfEventPanel cannot react to changes in the database 
+ *  or the ArrayList of RequirementEvents within it (the "events" variable) without being
+ *  re-created, so a new ListOfEventPanel should be created whenever changes occur.
+ */
 @SuppressWarnings("serial")
 public class ListOfEventPanel extends JPanel {
 	
+	/** An ArrayList of RequirementEvents that will hold the list of RequirementEvents associated with this requirement */ 
 	private ArrayList<RequirementEvent> events;
 	
-
+	/** The basic constructor. This constructs the ListOfEventPanel,
+	 * adding a new EventPanel for every event in the current requirement
+	 * (passed in as "currentRequirement"). Please note that the ListOfEventPanel
+	 * cannot react to changes in the database or the "events" variable without being
+	 * re-created, so a new ListOfEventPanel should be created whenever changes occur.
+	 * 
+	 * @param currentRequirement The Requirement whose events are to be displayed in this panel
+	 */
 	public ListOfEventPanel(Requirement currentRequirement) {
-		
 		// Create and set the layout manager for this panel
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		
@@ -44,7 +56,11 @@ public class ListOfEventPanel extends JPanel {
 		//Set the noteListModel to the NoteListModel passed in as "newModel"
 		events = currentRequirement.getEvents();
 		
-		double totalHeight=0; //This stores a running total of the heights of the note panels- this is used later to set the preferred size of this panel appropriately so that the scroll pane that will contain this panel can scroll appropriately
+		/** This stores a running total of the heights of the note panels-
+		 *  this is used later to set the preferred size of this panel appropriately
+		 *  so that the scroll pane that will contain this ListOfEventPanel can scroll appropriately
+		 */
+		double totalHeight=0; 
 		
 		//For each note in the noteListModel....
 		for (int i = events.size()-1; i>=0; i--)
@@ -57,11 +73,11 @@ public class ListOfEventPanel extends JPanel {
 
 		this.setBackground(Color.WHITE); //set the background color of this panel to white
 		
-
 		this.setPreferredSize(new Dimension(325, (int)totalHeight)); //Set the preferred size of this panel 
 		
 		//Add resize functionality to keep the maximum height current
-		final JPanel referenceToThis = this;// a reference to this panel to use within the following constructor
+		/** A reference to this ListOfEventPanel to use within the componentResized componentListener */
+		final JPanel referenceToThis = this;
 		//Construct and add a new component listener to listen for a resize event
 		this.addComponentListener(new ComponentAdapter() {
 			public void componentResized(ComponentEvent e) {  //on resize...
@@ -71,9 +87,8 @@ public class ListOfEventPanel extends JPanel {
 		//end resize functionality
 	}
 
-	/**
-	 * This function is called when the ListOfEventPanel is resized or when 
-	 * any of the EventPanels is resized 
+	/** This function is called when the ListOfEventPanel is resized or when 
+	 * any of the EventPanels within the ListOfEventPanel is resized 
 	 */
 	public void resizeFunction(){
 		double newTotalHeight=0;//This stores the new running total of the heights of the event panels- this is used later to set the preferred size of this panel appropriately so that the scroll pane that will contain this panel can scroll appropriately
@@ -82,6 +97,5 @@ public class ListOfEventPanel extends JPanel {
 		}
 		this.setPreferredSize(new Dimension(325, (int)newTotalHeight)); //Set the preferred size of this panel appropriately so that the scroll pane that will contain this panel can scroll appropriately 
 	}
-	
 	
 }
