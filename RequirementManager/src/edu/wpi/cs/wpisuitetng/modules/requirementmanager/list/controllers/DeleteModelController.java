0f1cd5bb4b439a05480b327ed40e77a431bad96c
Edison Jimenez <ejimenez@wpi.edu>
2013-04-25 22:39:14 -0400

    Audit Fixes
    Fixed "Unnecessary Type Cast"
    Fixed "One Statement Per Line"

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/controllers/DeleteModelController.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/controllers/DeleteModelController.java
@@ -111,7 +111,8 @@ public class DeleteModelController implements ActionListener {
 
 	/** Triggers a refresh of all list views, starting with the view that holds this controller.
 	 *  If that doesn't work, the builder panel is tried, if both fail, an error message is printed. */
-	public void refreshListViews() {;
+	public void refreshListViews() {
+	
 		// Try to refresh all from the list, if that doesn't work
 		if (!listView.refreshAll())	{	
 			System.err.println("Fail: cannot refresh views after deleting a" + modelName);
