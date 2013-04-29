6c54e27dc4855bcb7a0b8b7e7b3c1e139b901006
Mike Calder <mrcalder@wpi.edu>
2013-04-29 02:48:14 -0400

    Edit Mode Builder Finished

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/views/EditModeBuilderPanel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/list/views/EditModeBuilderPanel.java
@@ -22,18 +22,13 @@ import javax.swing.SpringLayout;
 
 @SuppressWarnings("serial")
 public class EditModeBuilderPanel extends JPanel {
-	
-	/** The panel for the enter label */
-	private JPanel enterPanel;
-	
+
 	/** The panel for the legend */
 	private JPanel legendPanel;
-	
+
 	/** The panel for the warnings */
 	private JPanel warningPanel;
-	
 
-	
 	/** The white label */
 	private JTextField whiteLabel;
 	/** The gray label */
@@ -42,7 +37,7 @@ public class EditModeBuilderPanel extends JPanel {
 	private JTextField yellowLabel;
 	/** The red label */
 	private JTextField redLabel;
-	
+
 	/** The error 1 label */
 	private JLabel err1Label;
 	/** The error 2 label */
@@ -51,7 +46,7 @@ public class EditModeBuilderPanel extends JPanel {
 	private JLabel err3Label;
 	/** The enter label */
 	private JLabel enterLabel;
-	
+
 	/** The layout manager for this panel */
 	protected SpringLayout layout;
 
@@ -59,60 +54,60 @@ public class EditModeBuilderPanel extends JPanel {
 	 * @param view The ListTab that this panel will live in
 	 */
 	public EditModeBuilderPanel(ListTab view) {
-		
+
 		//layout for this panel
 		layout  = new SpringLayout();
 		this.setLayout(layout);
-		
-		enterPanel = new JPanel();
-		enterPanel.setPreferredSize(new Dimension(100, 95));
-		legendPanel = new JPanel();
-		legendPanel.setPreferredSize(new Dimension(110, 95));
-		warningPanel = new JPanel();
-		
 
-		
+		legendPanel = new JPanel();
+		legendPanel.setPreferredSize(new Dimension(220, 40));
+		warningPanel = new JPanel();
+
 		// Legend Panel
-		whiteLabel = new JTextField("    Editable field");
+		whiteLabel = new JTextField("Editable");
+		whiteLabel.setHorizontalAlignment(JTextField.CENTER);
 		whiteLabel.setEditable(false);
 		whiteLabel.setPreferredSize(new Dimension(105, 17));
 		whiteLabel.setBackground(Color.white);
-		grayLabel = new JTextField("Non-editable field");
+		grayLabel = new JTextField("Non-Editable");
+		grayLabel.setHorizontalAlignment(JTextField.CENTER);
 		grayLabel.setEditable(false);
 		grayLabel.setPreferredSize(new Dimension(105, 17));
 		grayLabel.setBackground(Color.lightGray);
-		yellowLabel = new JTextField("    Modified field");
+		yellowLabel = new JTextField("Modified");
+		yellowLabel.setHorizontalAlignment(JTextField.CENTER);
 		yellowLabel.setEditable(false);
 		yellowLabel.setPreferredSize(new Dimension(105, 17));
 		yellowLabel.setBackground(new Color(248,253,188));
-		redLabel = new JTextField("      Invalid field");
+		redLabel = new JTextField("Invalid");
+		redLabel.setHorizontalAlignment(JTextField.CENTER);
 		redLabel.setEditable(false);
 		redLabel.setPreferredSize(new Dimension(105, 17));
 		redLabel.setBackground(new Color(255,50,50));
-		legendPanel.add(grayLabel);
-		legendPanel.add(whiteLabel);
-		legendPanel.add(yellowLabel);
-		legendPanel.add(redLabel);
-		
+
 		// Set legend constraints
 		SpringLayout legendLayout = new SpringLayout();
 		legendPanel.setLayout(legendLayout);
-		// Setup vertical constraints
+		
+		// Setup constraints
 		legendLayout.putConstraint(SpringLayout.NORTH, whiteLabel, 0, SpringLayout.NORTH, legendPanel);
-		legendLayout.putConstraint(SpringLayout.NORTH, grayLabel, 15, SpringLayout.SOUTH, whiteLabel);
-		
-		legendLayout.putConstraint(SpringLayout.NORTH, yellowLabel, 0, SpringLayout.NORTH, legendPanel);
-		legendLayout.putConstraint(SpringLayout.NORTH, redLabel, 15, SpringLayout.SOUTH, yellowLabel);
-
-		
-		// Setup horizontal constraints
 		legendLayout.putConstraint(SpringLayout.WEST, whiteLabel, 0, SpringLayout.WEST, legendPanel);
-		legendLayout.putConstraint(SpringLayout.WEST, yellowLabel, 15, SpringLayout.EAST, whiteLabel);
 
-		legendLayout.putConstraint(SpringLayout.WEST, grayLabel, 0, SpringLayout.EAST, legendPanel);
-		legendLayout.putConstraint(SpringLayout.WEST, redLabel, 15, SpringLayout.EAST, grayLabel);
-
+		legendLayout.putConstraint(SpringLayout.NORTH, yellowLabel, 0, SpringLayout.NORTH, legendPanel);
+		legendLayout.putConstraint(SpringLayout.EAST, yellowLabel, 0, SpringLayout.EAST, legendPanel);
 		
+		legendLayout.putConstraint(SpringLayout.SOUTH, grayLabel, 0, SpringLayout.SOUTH, legendPanel);
+		legendLayout.putConstraint(SpringLayout.WEST, grayLabel, 0, SpringLayout.WEST, legendPanel);
+		
+		legendLayout.putConstraint(SpringLayout.SOUTH, redLabel, 0, SpringLayout.SOUTH, legendPanel);
+		legendLayout.putConstraint(SpringLayout.EAST, redLabel, 0, SpringLayout.EAST, legendPanel);
+
+		legendPanel.add(whiteLabel);
+		legendPanel.add(yellowLabel);
+		legendPanel.add(grayLabel);
+		legendPanel.add(redLabel);
+
+
 		// Warning panel
 		err1Label = new JLabel();
 		err1Label.setForeground(Color.red);
@@ -128,16 +123,24 @@ public class EditModeBuilderPanel extends JPanel {
 		err3Label.setFont(err1Label.getFont().deriveFont(11));
 		enterLabel = new JLabel();
 		enterLabel.setText("Press enter to finalize a change.");
-		enterLabel.setFont(enterLabel.getFont().deriveFont(13));				
-		warningPanel.add(enterLabel);
-		warningPanel.add(err1Label);
-		warningPanel.add(err2Label);
-		warningPanel.add(err3Label);
+		enterLabel.setFont(enterLabel.getFont().deriveFont(13));
 
 		// "Edit Mode" panel
 		JLabel editModeLabel = new JLabel();
 		editModeLabel.setText("Edit Mode");
-		editModeLabel.setFont(new Font("Arial Bold",3,40));
+		editModeLabel.setFont(new Font("Arial Bold",1,40));
+
+		// Arrange all panels
+		layout.putConstraint(SpringLayout.NORTH, editModeLabel, 0, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.SOUTH, editModeLabel, 0, SpringLayout.SOUTH, this);
+		layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, editModeLabel, 0, SpringLayout.HORIZONTAL_CENTER, this);
+		layout.putConstraint(SpringLayout.NORTH, warningPanel, 10, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.SOUTH, warningPanel, 0, SpringLayout.SOUTH, this);
+		layout.putConstraint(SpringLayout.WEST, warningPanel, 60, SpringLayout.EAST, editModeLabel);
+		layout.putConstraint(SpringLayout.EAST, warningPanel, 0, SpringLayout.EAST, this);
+		layout.putConstraint(SpringLayout.VERTICAL_CENTER, legendPanel, 0, SpringLayout.VERTICAL_CENTER, this);
+		layout.putConstraint(SpringLayout.EAST, legendPanel, -60, SpringLayout.WEST, editModeLabel);
+
 
 		//Setting up layout of warning messages
 		SpringLayout warningLayout = new SpringLayout();
@@ -148,35 +151,28 @@ public class EditModeBuilderPanel extends JPanel {
 		warningLayout.putConstraint(SpringLayout.NORTH, err2Label, 10, SpringLayout.SOUTH, err1Label);
 		warningLayout.putConstraint(SpringLayout.NORTH, err3Label, 10, SpringLayout.SOUTH, err2Label);
 
-		
-		// Arrange all panels
-		layout.putConstraint(SpringLayout.NORTH, legendPanel, 0, SpringLayout.NORTH, this);
-		layout.putConstraint(SpringLayout.WEST, legendPanel, 0, SpringLayout.WEST, this);
-		layout.putConstraint(SpringLayout.SOUTH, legendPanel, 0, SpringLayout.SOUTH, this);
-		layout.putConstraint(SpringLayout.WEST, editModeLabel, 15, SpringLayout.EAST, legendPanel);
-		layout.putConstraint(SpringLayout.NORTH, editModeLabel, 0, SpringLayout.NORTH, this);
-		layout.putConstraint(SpringLayout.SOUTH, editModeLabel, 0, SpringLayout.SOUTH, this);
-		layout.putConstraint(SpringLayout.NORTH, warningPanel, 0, SpringLayout.NORTH, this);
-		layout.putConstraint(SpringLayout.SOUTH, warningPanel, 0, SpringLayout.SOUTH, this);
-		layout.putConstraint(SpringLayout.WEST, warningPanel, 50, SpringLayout.EAST, editModeLabel);
-		layout.putConstraint(SpringLayout.EAST, warningPanel, 0, SpringLayout.EAST, this);
-		
+		warningPanel.add(enterLabel);
+		warningPanel.add(err1Label);
+		warningPanel.add(err2Label);
+		warningPanel.add(err3Label);
+
+
 		this.add(legendPanel);
 		this.add(editModeLabel);
 		this.add(warningPanel);
 	}
-	
+
 	/** Takes an array of strings and puts them into labels
 	 * 
 	 * @param errorMessages an array of messages to show
 	 */
 	public void setInvalidInputMessages(String[] errorMessages) {
-		
+
 		int errorSize;
 		errorSize = errorMessages.length;
-		
+
 		String errorString = "";
-		
+
 		for(int i = 0; i < errorSize; i++) {
 			errorString = errorMessages[i];
 			if (i == 0) {
@@ -189,6 +185,6 @@ public class EditModeBuilderPanel extends JPanel {
 				err3Label.setText(errorString);
 			}
 		}
-		
+
 	}
 }
