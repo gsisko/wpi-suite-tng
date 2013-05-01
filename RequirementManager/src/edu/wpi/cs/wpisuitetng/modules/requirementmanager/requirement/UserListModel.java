b1a1d84b335601a7a93ca2b6c683fe111d0ee80e
Robert Smieja <robertsmieja@gmail.com>
2013-05-01 02:55:50 -0400

    Boy, we sure could use more COMMENT UPDATES

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/requirement/UserListModel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/requirement/UserListModel.java
@@ -17,6 +17,9 @@ import java.util.Iterator;
 
 import javax.swing.AbstractListModel;
 
+/**
+ * The model for containing a list of users
+ */
 @SuppressWarnings({"serial","rawtypes"})
 public class UserListModel extends AbstractListModel {
 	
@@ -26,11 +29,19 @@ public class UserListModel extends AbstractListModel {
 		users = new ArrayList<String>();
 	}
 	
+	/**
+	 * Adds the given user to the list
+	 * @param newUser String
+	 */
 	public void addUser(String newUser) {
 		users.add(newUser);// Add the user
 		this.fireIntervalAdded(this, 0, 0);// Notify the model that it has changed so the GUI will be updated
 	}
 	
+	/**
+	 * Adds an array of given users to the list
+	 * @param arrayOfUsers String[]
+	 */
 	public void addUsers(String[] arrayOfUsers) {
 		for (int i = 0; i < arrayOfUsers.length; i++) {
 			users.add(arrayOfUsers[i]);
@@ -48,19 +59,39 @@ public class UserListModel extends AbstractListModel {
 		this.fireIntervalRemoved(this, 0, Math.max(oldSize - 1, 0));
 	}
 
+	/**
+	 * Gets a user at the given index
+	 * @param index int
+	 * @return String
+	 * @see javax.swing.ListModel#getElementAt(int)
+	 */
 	@Override
 	public String getElementAt(int index) {
 		return users.get(users.size() - 1 - index).toString();
 	}
 	
+	/**
+	 * Gets a user at the given index
+	 * @param index int
+	 * @return String
+	 */
 	public String getUserAt(int index) {
 		return users.get(users.size() - 1 - index);
 	}
 	
+	/**
+	 * Removes the user at the given index
+	 * @param index int
+	 */
 	public void removeElementAt(int index) {
 		users.remove(users.size() - 1 - index);
 	}
 
+	/**
+	 * Returns the size of the list
+	 * @return int
+	 * @see javax.swing.ListModel#getSize()
+	 */
 	@Override
 	public int getSize() {
 		return users.size();
